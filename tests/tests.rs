2e83c6a5eb4300abea2063db8ff73eb666eeaf8a
root <root@localhost>
2015-01-05 05:27:30 +0100

    Add test for ograph

---
+++ tests/tests.rs
@@ -0,0 +1,83 @@
+
+extern crate petgraph;
+
+use petgraph::{
+    OGraph,
+    BreadthFirst,
+    dijkstra,
+};
+
+use petgraph::ograph::toposort;
+
+
+#[test]
+fn ograph_1()
+{
+    let mut og = OGraph::new();
+    let a = og.add_node(0i);
+    let b = og.add_node(1i);
+    let c = og.add_node(2i);
+    let d = og.add_node(3i);
+    let ed1 = og.add_edge(a, b, 0i);
+    let ed2 = og.add_edge(a, c, 1);
+    og.add_edge(c, a, 2);
+    og.add_edge(a, a, 3);
+    og.add_edge(b, c, 4);
+    og.add_edge(b, a, 5);
+    og.add_edge(a, d, 6);
+    assert_eq!(og.node_count(), 4);
+    assert_eq!(og.edge_count(), 7);
+
+    assert!(og.find_edge(a, b).is_some());
+    assert!(og.find_edge(d, a).is_none());
+    assert!(og.find_edge(a, a).is_some());
+
+    for no in og.edges(a) {
+        println!("Edges {}", no);
+    }
+
+    for no in og.edges_both(a) {
+        println!("EdgesBoth {}", no);
+    }
+
+    println!("{}", og);
+    println!("Remove {}", a);
+    for no in BreadthFirst::new(&og, a) {
+        println!("Visit {}", no);
+    }
+    og.remove_node(a);
+    assert_eq!(og.node_count(), 3);
+    assert_eq!(og.edge_count(), 1);
+    assert!(og.find_edge(a, b).is_none());
+    assert!(og.find_edge(d, a).is_none());
+    assert!(og.find_edge(a, a).is_none());
+    assert!(og.find_edge(b, c).is_some());
+}
+
+#[test]
+fn ograph_2()
+{
+    let mut g = OGraph::<_, f32>::new();
+    let a = g.add_node("A");
+    let b = g.add_node("B");
+    let c = g.add_node("C");
+    let d = g.add_node("D");
+    let e = g.add_node("E");
+    let f = g.add_node("F");
+    g.add_edge(a, b, 7.);
+    g.add_edge(a, c, 9.);
+    g.add_edge(a, d, 14.);
+    g.add_edge(b, c, 10.);
+    g.add_edge(c, d, 2.);
+    g.add_edge(d, e, 9.);
+    g.add_edge(b, f, 15.);
+    g.add_edge(c, f, 11.);
+    g.add_edge(e, f, 6.);
+    let scores = dijkstra(&g, a, |gr, n| gr.edges(n).map(|(n, &e)| (n, e)));
+    assert_eq!(scores[f], 20.);
+
+    let x = g.add_node("X");
+    let y = g.add_node("Y");
+    g.add_edge(x, y, 0.);
+    println!("{}", toposort(&g));
+}
