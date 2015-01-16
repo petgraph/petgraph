38fcb7602432e33fb41ead7a112378981388a667
root <root@localhost>
2015-01-16 22:11:58 +0100

    Fix compilation of benches

--- benches/ograph.rs
+++ benches/ograph.rs
@@ -8,8 +8,8 @@ use petgraph::graph::Graph;
 #[bench]
 fn bench_inser(b: &mut test::Bencher) {
     let mut og = Graph::new();
-    let fst = og.add_node(0);
-    for x in range(1, 125) {
+    let fst = og.add_node(0i32);
+    for x in (1..125) {
         let n = og.add_node(x);
         og.add_edge(fst, n, ());
     }
@@ -23,16 +23,16 @@ fn bench_remove(b: &mut test::Bencher) {
     // removal is very slow in a big graph.
     // and this one doesn't even have many nodes.
     let mut og = Graph::new();
-    let fst = og.add_node(0);
+    let fst = og.add_node(0i32);
     let mut prev = fst;
-    for x in range(1, 1250) {
+    for x in (1..1250) {
         let n = og.add_node(x);
         og.add_edge(prev, n, ());
         prev = n;
     }
     //println!("{}", og);
     b.iter(|| {
-        for _ in range(0, 100) {
+        for _ in (0 .. 100) {
             og.remove_node(fst);
         }
     })
