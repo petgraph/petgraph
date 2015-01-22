1c8e2298364ecbcb2e59d3ff3a68da1a585aa249
root <root@localhost>
2015-01-22 17:54:02 +0100

    UnionFind: simplify into_labeling

--- src/unionfind.rs
+++ src/unionfind.rs
@@ -145,13 +145,14 @@ impl<K> UnionFind<K> where K: num::UnsignedInt
     }
 
     /// Return a vector mapping each element to its representative.
-    pub fn into_labeling(mut self) -> Vec<K> where
-        K: num::NumCast,
+    pub fn into_labeling(mut self) -> Vec<K>
     {
         // write in the labeling of each element
         unsafe {
             for ix in (0..self.parent.len()) {
-                self.find_mut_recursive(num::cast(ix).unwrap());
+                let k = *get_unchecked(&self.parent[], ix);
+                let xrep = self.find_mut_recursive(k);
+                *self.parent.get_unchecked_mut(ix) = xrep;
             }
         }
         self.parent
