1c22789c2402adfd5b98dbbaaf83321bce2a2226
bluss <bluss@users.noreply.github.com>
2015-11-25 17:41:44 +0100

    Add example for Graph with illustration

--- src/graph.rs
+++ src/graph.rs
@@ -205,6 +205,23 @@ impl<E, Ix: IndexType = DefIndex> Edge<E, Ix>
 ///
 /// Based on the graph datastructure used in rustc.
 ///
+/// ```
+/// use petgraph::Graph;
+/// 
+/// let mut deps = Graph::<&str, &str>::new();
+/// let pg = deps.add_node("petgraph");
+/// let fb = deps.add_node("fixedbitset");
+/// let qc = deps.add_node("quickcheck");
+/// let rand = deps.add_node("rand");
+/// let libc = deps.add_node("libc");
+/// deps.extend_with_edges(&[
+///     (pg, fb), (pg, qc),
+///     (qc, rand), (rand, libc), (qc, libc),
+/// ]);
+/// ```
+///
+/// ![graph-example](graph-example.svg)
+///
 /// ### Graph Indices
 ///
 /// The graph maintains unique indices for nodes and edges, and node and edge
