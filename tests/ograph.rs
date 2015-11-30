af1aa98ac0709d730e97db3c74059dbc1340eb4e
bluss <bluss@users.noreply.github.com>
2015-11-30 20:08:46 +0100

    Fix neighbor and edge iterators visiting self loops twice
    Fixes #31
    
    For an undirected edges traversal, selfloops would be visited twice
    since the same edge is of course linked in both in the outgoing as well
    as incoming edge lists.
    
    Fix the code in the Edges iterator, the Neighbors iterator is simply
    equivalent, and we make it a wrapper of Edges.
    
    [breaking-change]

--- tests/ograph.rs
+++ tests/ograph.rs
@@ -979,3 +979,67 @@ fn assert_graph_consistent<N, E, Ty, Ix>(g: &Graph<N, E, Ty, Ix>)
                 "Edge not in graph! {:?} to {:?}", edge.source(), edge.target());
     }
 }
+
+#[test]
+fn neighbors_selfloops() {
+    // Directed graph
+    let mut gr = Graph::<_ ,()>::new();
+    let a = gr.add_node("a");
+    let b = gr.add_node("b");
+    let c = gr.add_node("c");
+    gr.extend_with_edges(&[
+        (a, a),
+        (a, b),
+        (c, a),
+        (a, a),
+    ]);
+
+    let out_edges = [a, a, b];
+    let in_edges = [a, a, c];
+    let undir_edges = [a, a, b, c];
+    let mut seen_out = gr.neighbors(a).collect::<Vec<_>>();
+    seen_out.sort();
+    assert_eq!(&seen_out, &out_edges);
+    let mut seen_in = gr.neighbors_directed(a, Incoming).collect::<Vec<_>>();
+    seen_in.sort();
+    assert_eq!(&seen_in, &in_edges);
+
+    let mut seen_undir = gr.neighbors_undirected(a).collect::<Vec<_>>();
+    seen_undir.sort();
+    assert_eq!(&seen_undir, &undir_edges);
+
+    let mut seen_undir = gr.edges_both(a).map(|(x, _)| x).collect::<Vec<_>>();
+    seen_undir.sort();
+    assert_eq!(&seen_undir, &undir_edges);
+
+    // Undirected graph
+    let mut gr: Graph<_, (), _> = Graph::new_undirected();
+    let a = gr.add_node("a");
+    let b = gr.add_node("b");
+    let c = gr.add_node("c");
+    gr.extend_with_edges(&[
+        (a, a),
+        (a, b),
+        (c, a),
+    ]);
+
+    let out_edges = [a, b, c];
+    let in_edges = [a, b, c];
+    let undir_edges = [a, b, c];
+    let mut seen_out = gr.neighbors(a).collect::<Vec<_>>();
+    seen_out.sort();
+    assert_eq!(&seen_out, &out_edges);
+
+    let mut seen_out = gr.edges(a).map(|(x, _)| x).collect::<Vec<_>>();
+    seen_out.sort();
+    assert_eq!(&seen_out, &out_edges);
+
+    let mut seen_in = gr.neighbors_directed(a, Incoming).collect::<Vec<_>>();
+    seen_in.sort();
+    assert_eq!(&seen_in, &in_edges);
+
+    let mut seen_undir = gr.neighbors_undirected(a).collect::<Vec<_>>();
+    seen_undir.sort();
+    assert_eq!(&seen_undir, &undir_edges);
+}
+
