dec2cc1b664cdc3ad6eab53ea75fb27e1ec90483
bluss <bluss@users.noreply.github.com>
2015-12-09 02:21:10 +0100

    Add NeighborIter, NeighborsDirected impls for StableGraph

--- src/stable.rs
+++ src/stable.rs
@@ -448,3 +448,27 @@ fn stable_graph() {
         println!("edge {:?} -> {:?}", d, neigh);
     }
 }
+
+#[test]
+fn dfs() {
+    use Dfs;
+
+    let mut gr = StableGraph::<_, _>::with_capacity(0, 0);
+    let a = gr.add_node("a");
+    let b = gr.add_node("b");
+    let c = gr.add_node("c");
+    let d = gr.add_node("d");
+    gr.add_edge(a, b, 1);
+    gr.add_edge(a, c, 2);
+    gr.add_edge(b, c, 3);
+    gr.add_edge(b, d, 4);
+    gr.add_edge(c, d, 5);
+    gr.add_edge(d, b, 6);
+    gr.add_edge(c, b, 7);
+    println!("{:?}", gr);
+
+    let mut dfs = Dfs::new(&gr, a);
+    while let Some(next) = dfs.next(&gr) {
+        println!("dfs visit => {:?}, weight={:?}", next, &gr[next]);
+    }
+}
