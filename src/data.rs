d02bf9ea23a08e589a948f1fb95b0390d2905bf5
bluss <bluss@users.noreply.github.com>
2016-10-14 23:51:37 +0200

    Graph building traits for StableGraph

--- src/data.rs
+++ src/data.rs
@@ -2,6 +2,7 @@
 
 
 use Graph;
+use stable_graph::StableGraph;
 use ::{
     EdgeType,
 };
@@ -97,6 +98,30 @@ impl<N, E, Ty, Ix> DataMapMut for Graph<N, E, Ty, Ix>
     }
 }
 
+impl<N, E, Ty, Ix> DataMap for StableGraph<N, E, Ty, Ix>
+    where Ty: EdgeType,
+          Ix: IndexType
+{
+    fn node_weight(&self, id: Self::NodeId) -> Option<&Self::NodeWeight> {
+        self.node_weight(id)
+    }
+    fn edge_weight(&self, id: Self::EdgeId) -> Option<&Self::EdgeWeight> {
+        self.edge_weight(id)
+    }
+}
+
+impl<N, E, Ty, Ix> DataMapMut for StableGraph<N, E, Ty, Ix>
+    where Ty: EdgeType,
+          Ix: IndexType
+{
+    fn node_weight_mut(&mut self, id: Self::NodeId) -> Option<&mut Self::NodeWeight> {
+        self.node_weight_mut(id)
+    }
+    fn edge_weight_mut(&mut self, id: Self::EdgeId) -> Option<&mut Self::EdgeWeight> {
+        self.edge_weight_mut(id)
+    }
+}
+
 impl<N, E, Ty, Ix> Build for Graph<N, E, Ty, Ix>
     where Ty: EdgeType,
           Ix: IndexType,
@@ -120,7 +145,39 @@ impl<N, E, Ty, Ix> Build for Graph<N, E, Ty, Ix>
     }
 }
 
+impl<N, E, Ty, Ix> Build for StableGraph<N, E, Ty, Ix>
+    where Ty: EdgeType,
+          Ix: IndexType,
+{
+    fn add_node(&mut self, weight: Self::NodeWeight) -> Self::NodeId {
+        self.add_node(weight)
+    }
+    fn add_edge(&mut self,
+                a: Self::NodeId,
+                b: Self::NodeId,
+                weight: Self::EdgeWeight) -> Option<Self::EdgeId>
+    {
+        Some(self.add_edge(a, b, weight))
+    }
+    fn update_edge(&mut self,
+                   a: Self::NodeId,
+                   b: Self::NodeId,
+                   weight: Self::EdgeWeight) -> Self::EdgeId
+    {
+        self.update_edge(a, b, weight)
+    }
+}
+
 impl<N, E, Ty, Ix> Create for Graph<N, E, Ty, Ix>
+    where Ty: EdgeType,
+          Ix: IndexType,
+{
+    fn with_capacity(nodes: usize, edges: usize) -> Self {
+        Self::with_capacity(nodes, edges)
+    }
+}
+
+impl<N, E, Ty, Ix> Create for StableGraph<N, E, Ty, Ix>
     where Ty: EdgeType,
           Ix: IndexType,
 {
@@ -181,6 +238,18 @@ fn from_elements_indexable<G, I>(iterable: I) -> G
 }
 
 impl<N, E, Ty, Ix> FromElements for Graph<N, E, Ty, Ix>
+    where Ty: EdgeType,
+          Ix: IndexType,
+{
+    fn from_elements<I>(iterable: I) -> Self
+        where Self: Sized,
+              I: IntoIterator<Item=Element<Self::NodeWeight, Self::EdgeWeight>>,
+    {
+        from_elements_indexable(iterable)
+    }
+}
+
+impl<N, E, Ty, Ix> FromElements for StableGraph<N, E, Ty, Ix>
     where Ty: EdgeType,
           Ix: IndexType,
 {
