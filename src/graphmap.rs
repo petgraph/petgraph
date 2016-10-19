5001e12cad6023f3a4c3bbc0a95b05b43d2c5d17
Alex Lohr <alexthkloss@web.de>
2016-10-19 15:05:48 +0200

    addressed comments

--- src/graphmap.rs
+++ src/graphmap.rs
@@ -130,7 +130,11 @@ impl<N, E, Ty> GraphMap<N, E, Ty>
     /// Use their natual order to map the node pair (a, b) to a canonical edge id.
     #[inline]
     fn edge_key(a: N, b: N) -> (N, N) {
-        if Ty::is_directed() || a <= b { (a, b) } else { (b, a) }
+        if Ty::is_directed() {
+            (a, b)
+        } else {
+            if a <= b { (a, b) } else { (b, a) }
+        }
     }
 
     /// Whether the graph has directed edges.
