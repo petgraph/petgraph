9aa61e3486df4c8374fca20264b798116a1f2c17
Ruben Lapauw <Ruben.Lapauw@cs.kuleuven.be>
2017-09-07 18:11:55 +0200

    Simpler type constraints for intoiterator

--- src/into_elements.rs
+++ src/into_elements.rs
@@ -1,36 +1,32 @@
 use data::{DataMap, Element, FromElements};
-use visit::{EdgeRef, Data, GraphBase, IntoNodeIdentifiers, IntoEdgeReferences};
+use visit::{EdgeRef, Data, GraphBase,  IntoNodeIdentifiers, IntoEdgeReferences};
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::hash::Hash;
 use std::rc::Rc;
 
 /// Turn a graph with clonable weights that implementings the correct taits into its Elements.
-pub struct IntoElements<'a, G>(pub &'a G) where G: 'a;
+pub struct IntoElements<'a, G: 'a>(pub &'a G);
 
 
-//Todo: weights do not need to be cloneable. I couldn't get the lifetimes of EdgeWeight to work.
+//Todo: ids do not need to be cloneable. I couldn't get the lifetimes of EdgeWeight to work.
 /// Graphs that have a Cloneable weigths and implement the necessary traits can be deconstructed into elements.
 impl<'a, G> IntoIterator for IntoElements<'a, G>
-    where G: 'a + DataMap + Data,
-          &'a G: IntoNodeIdentifiers + IntoEdgeReferences,
-          &'a G: GraphBase<NodeId = G::NodeId, EdgeId = G::EdgeId>,
-          &'a G: Data<EdgeWeight = G::EdgeWeight>,
-          G::NodeWeight: Clone,
-          G::EdgeWeight: Clone,
-          G::NodeId: Eq + Hash + Clone,
-          G::EdgeId: Clone
+    where &'a G: GraphBase + DataMap + IntoNodeIdentifiers + IntoEdgeReferences,
+          <&'a G as Data>::NodeWeight: Clone,
+          <&'a G as Data>::EdgeWeight: Clone,
+          <&'a G as GraphBase>::NodeId: Eq + Hash + Clone,
 {
-    type Item = Element<G::NodeWeight, G::EdgeWeight>;
+    type Item = Element<<&'a G as Data>::NodeWeight, <&'a G as Data>::EdgeWeight>;
     type IntoIter = Box<Iterator<Item = Self::Item> + 'a>;
     fn into_iter(self) -> Self::IntoIter {
         use std::iter;
         let nodes = self.0.node_identifiers();
         let edge_refs = self.0.edge_references();
-        let map = Rc::new(RefCell::new(HashMap::<G::NodeId, usize>::new()));
+        let map = Rc::new(RefCell::new(HashMap::<<&'a G as GraphBase>::NodeId, usize>::new()));
         let nodes: iter::Map<<&'a G as IntoNodeIdentifiers>::NodeIdentifiers, _> = {
             let map = map.clone();
-            nodes.map(move |ni: G::NodeId| {
+            nodes.map(move |ni: <&'a G as GraphBase>::NodeId| {
                 let len = map.borrow().len();
                 map.borrow_mut().insert(ni.clone(), len);
                 Element::Node {
@@ -57,16 +53,12 @@ impl<'a, G> IntoIterator for IntoElements<'a, G>
 
 /// Rebuild a graph with Cloneable weights into another graph.
 pub fn rebuild<'a, G, H>(g: &'a G) -> H
-    where G: 'a + DataMap + Data,
-          &'a G: IntoNodeIdentifiers + IntoEdgeReferences,
-          &'a G: GraphBase<NodeId = G::NodeId, EdgeId = G::EdgeId>,
-          &'a G: Data<EdgeWeight = G::EdgeWeight>,
-          G::NodeWeight: Clone,
-          G::EdgeWeight: Clone,
-          G::NodeId: Eq + Hash + Clone,
-          G::EdgeId: Clone,
+    where &'a G: GraphBase + DataMap + IntoNodeIdentifiers + IntoEdgeReferences,
+          <&'a G as Data>::NodeWeight: Clone,
+          <&'a G as Data>::EdgeWeight: Clone,
+          <&'a G as GraphBase>::NodeId: Eq + Hash + Clone,
           H: FromElements,
-          H: Data<NodeWeight = G::NodeWeight, EdgeWeight = G::EdgeWeight>
+          H: Data<NodeWeight = <&'a G as Data>::NodeWeight, EdgeWeight = <&'a G as Data>::EdgeWeight>
 {
     let mut elements = IntoElements(g).into_iter();
     return H::from_elements(&mut elements);
