2ea514d503820080b3f1c78a1189c59b2c9e923f
John Heitmann <jheitmann@gmail.com>
2019-12-09 17:18:17 -0800

    Switch to an example that uses Graph instead of GraphMap

--- src/lib.rs
+++ src/lib.rs
@@ -12,35 +12,35 @@
 //! # Example
 //! 
 //! ```rust
-//! use petgraph::graphmap::UnGraphMap;
+//! use petgraph::graph::{NodeIndex, UnGraph};
 //! use petgraph::algo::{dijkstra, min_spanning_tree};
 //! use petgraph::data::FromElements;
 //! use petgraph::dot::{Dot, Config};
 //!
-//! // Create an undirected graph where nodes are &str, and edges have no weight.
-//! let g = UnGraphMap::<&str, ()>::from_edges(&[
-//!     ("a", "b"), ("b", "c"), ("c", "d"),
-//!     ("a", "d")]);
+//! // Create an undirected graph with `i32` nodes and edges with `()` associated data.
+//! let g = UnGraph::<i32, ()>::from_edges(&[
+//!     (1, 2), (2, 3), (3, 4),
+//!     (1, 4)]);
 //!
-//! // Find the shortest path from "a" to "d" using 1 as the cost for every edge.
-//! let node_map = dijkstra(&g, "a", Some("d"), |_| 1);
-//! assert_eq!(&1, node_map.get("d").unwrap());
+//! // Find the shortest path from `1` to `4` using `1` as the cost for every edge.
+//! let node_map = dijkstra(&g, 1.into(), Some(4.into()), |_| 1);
+//! assert_eq!(&1i32, node_map.get(&NodeIndex::new(4)).unwrap());
 //!
 //! // Get the minimum spanning tree of the graph as a new graph, and check that
 //! // one edge was trimmed.
-//! let mst = UnGraphMap::from_elements(min_spanning_tree(&g));
-//! assert_eq!(g.all_edges().count() - 1, mst.all_edges().count());
+//! let mst = UnGraph::<_, _>::from_elements(min_spanning_tree(&g));
+//! assert_eq!(g.raw_edges().len() - 1, mst.raw_edges().len());
 //!
-//! // Output the tree to graphviz DOT format
+//! // Output the tree to `graphviz` `DOT` format
 //! println!("{:?}", Dot::with_config(&mst, &[Config::EdgeNoLabel]));
 //! // graph {
-//! //     0 [label="\"a\""]
-//! //     1 [label="\"b\""]
-//! //     2 [label="\"c\""]
-//! //     3 [label="\"d\""]
-//! //     0 -- 1
-//! //     2 -- 3
+//! //     0 [label="\"0\""]
+//! //     1 [label="\"0\""]
+//! //     2 [label="\"0\""]
+//! //     3 [label="\"0\""]
 //! //     1 -- 2
+//! //     3 -- 4
+//! //     2 -- 3
 //! // }
 //! ```
 //!
