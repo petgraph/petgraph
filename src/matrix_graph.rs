86dbe9c1f6c39f6137c370188e114375cec82aef
Franz <franz@miltz.me>
2020-04-27 10:21:41 +0100

    Fix typo in describing ensure_len function

--- src/matrix_graph.rs
+++ src/matrix_graph.rs
@@ -834,7 +834,7 @@ fn extend_lower_triangular_matrix<T: Default>(
     new_node_capacity + 1
 }
 
-/// Grow a Vec by appending the type's default value the `size` is reached.
+/// Grow a Vec by appending the type's default value until the `size` is reached.
 fn ensure_len<T: Default>(v: &mut Vec<T>, size: usize) {
     if let Some(n) = size.checked_sub(v.len()) {
         v.reserve(n);
