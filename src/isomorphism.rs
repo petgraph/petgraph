fa9a8b8092bfc40b52d2df652d13d22abb2402b6
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-08-24 12:13:15 +0200

    Make `isomorphism::Vf2State` store a reference to its graph

--- src/isomorphism.rs
+++ src/isomorphism.rs
@@ -2,15 +2,24 @@ use fixedbitset::FixedBitSet;
 use std::marker;
 
 use super::graph::{Graph, IndexType, NodeIndex};
-use super::{EdgeType, Incoming};
+use super::{EdgeType, Incoming, Outgoing};
 
+use super::visit::Data;
+use super::visit::GraphBase;
+use super::visit::GraphProp;
+use super::visit::EdgeCount;
+use super::visit::NodeCount;
+use super::visit::NodeCompactIndexable;
 use super::visit::GetAdjacencyMatrix;
+use super::visit::IntoNeighborsDirected;
 
 #[derive(Debug)]
-struct Vf2State<Ty, Ix> {
+struct Vf2State<'a, G: GetAdjacencyMatrix, Ix: IndexType> {
+    /// A reference to the graph this state was built from.
+    graph: &'a G,
     /// The current mapping M(s) of nodes from G0 → G1 and G1 → G0,
-    /// NodeIndex::end() for no mapping.
-    mapping: Vec<NodeIndex<Ix>>,
+    /// `<G::NodeId as IndexType>::max()` for no mapping.
+    mapping: Vec<Ix>,
     /// out[i] is non-zero if i is in either M_0(s) or Tout_0(s)
     /// These are all the next vertices that are not mapped yet, but
     /// have an outgoing edge from the mapping.
@@ -22,36 +31,28 @@ struct Vf2State<Ty, Ix> {
     ins: Vec<usize>,
     out_size: usize,
     ins_size: usize,
-    adjacency_matrix: FixedBitSet,
+    adjacency_matrix: G::AdjMatrix,
     generation: usize,
-    _etype: marker::PhantomData<Ty>,
 }
 
-impl<Ty, Ix> Vf2State<Ty, Ix>
+impl<'a, G, Ix> Vf2State<'a, G, Ix>
 where
-    Ty: EdgeType,
+    G: GetAdjacencyMatrix + GraphProp + NodeCount + IntoNeighborsDirected,
+    <G as GraphBase>::NodeId: IndexType,
     Ix: IndexType,
 {
-    pub fn new<N, E>(g: &Graph<N, E, Ty, Ix>) -> Self {
+    pub fn new(g: &'a G) -> Self {
         let c0 = g.node_count();
-        let mut state = Vf2State {
-            mapping: Vec::with_capacity(c0),
-            out: Vec::with_capacity(c0),
-            ins: Vec::with_capacity(c0 * (g.is_directed() as usize)),
+        Vf2State {
+            graph: g,
+            mapping: vec![<Ix as IndexType>::max(); c0],
+            out: vec![0; c0],
+            ins: vec![0; c0 * (g.is_directed() as usize)],
             out_size: 0,
             ins_size: 0,
             adjacency_matrix: g.adjacency_matrix(),
             generation: 0,
-            _etype: marker::PhantomData,
-        };
-        for _ in 0..c0 {
-            state.mapping.push(NodeIndex::end());
-            state.out.push(0);
-            if Ty::is_directed() {
-                state.ins.push(0);
-            }
         }
-        state
     }
 
     /// Return **true** if we have a complete mapping
@@ -60,28 +61,22 @@ where
     }
 
     /// Add mapping **from** <-> **to** to the state.
-    pub fn push_mapping<N, E>(
-        &mut self,
-        from: NodeIndex<Ix>,
-        to: NodeIndex<Ix>,
-        g: &Graph<N, E, Ty, Ix>,
-    ) {
+    pub fn push_mapping(&mut self, from: G::NodeId, to: Ix) {
         self.generation += 1;
-        let s = self.generation;
         self.mapping[from.index()] = to;
         // update T0 & T1 ins/outs
         // T0out: Node in G0 not in M0 but successor of a node in M0.
         // st.out[0]: Node either in M0 or successor of M0
-        for ix in g.neighbors(from) {
+        for ix in self.graph.neighbors_directed(from, Outgoing) {
             if self.out[ix.index()] == 0 {
-                self.out[ix.index()] = s;
+                self.out[ix.index()] = self.generation;
                 self.out_size += 1;
             }
         }
-        if g.is_directed() {
-            for ix in g.neighbors_directed(from, Incoming) {
+        if self.graph.is_directed() {
+            for ix in self.graph.neighbors_directed(from, Incoming) {
                 if self.ins[ix.index()] == 0 {
-                    self.ins[ix.index()] = s;
+                    self.ins[ix.index()] = self.generation;
                     self.ins_size += 1;
                 }
             }
@@ -89,28 +84,27 @@ where
     }
 
     /// Restore the state to before the last added mapping
-    pub fn pop_mapping<N, E>(&mut self, from: NodeIndex<Ix>, g: &Graph<N, E, Ty, Ix>) {
-        let s = self.generation;
-        self.generation -= 1;
-
+    pub fn pop_mapping<N, E>(&mut self, from: G::NodeId) {
         // undo (n, m) mapping
-        self.mapping[from.index()] = NodeIndex::end();
+        self.mapping[from.index()] = <Ix as IndexType>::max();
 
         // unmark in ins and outs
-        for ix in g.neighbors(from) {
-            if self.out[ix.index()] == s {
+        for ix in self.graph.neighbors_directed(from, Outgoing) {
+            if self.out[ix.index()] == self.generation {
                 self.out[ix.index()] = 0;
                 self.out_size -= 1;
             }
         }
-        if g.is_directed() {
-            for ix in g.neighbors_directed(from, Incoming) {
-                if self.ins[ix.index()] == s {
+        if self.graph.is_directed() {
+            for ix in self.graph.neighbors_directed(from, Incoming) {
+                if self.ins[ix.index()] == self.generation {
                     self.ins[ix.index()] = 0;
                     self.ins_size -= 1;
                 }
             }
         }
+
+        self.generation -= 1;
     }
 
     /// Find the next (least) node in the Tout set.
@@ -118,22 +112,22 @@ where
         self.out[from_index..]
             .iter()
             .enumerate()
-            .find(move |&(index, elt)| {
-                *elt > 0 && self.mapping[from_index + index] == NodeIndex::end()
+            .find(move |&(index, &elt)| {
+                elt > 0 && self.mapping[from_index + index] == <Ix as IndexType>::max()
             })
             .map(|(index, _)| index)
     }
 
     /// Find the next (least) node in the Tin set.
     pub fn next_in_index(&self, from_index: usize) -> Option<usize> {
-        if !Ty::is_directed() {
+        if !self.graph.is_directed() {
             return None;
         }
         self.ins[from_index..]
             .iter()
             .enumerate()
-            .find(move |&(index, elt)| {
-                *elt > 0 && self.mapping[from_index + index] == NodeIndex::end()
+            .find(move |&(index, &elt)| {
+                elt > 0 && self.mapping[from_index + index] == <Ix as IndexType>::max()
             })
             .map(|(index, _)| index)
     }
@@ -143,7 +137,7 @@ where
         self.mapping[from_index..]
             .iter()
             .enumerate()
-            .find(|&(_, elt)| *elt == NodeIndex::end())
+            .find(|&(_, &elt)| elt == <Ix as IndexType>::max())
             .map(|(index, _)| index)
     }
 }
@@ -159,17 +153,20 @@ where
 ///
 /// * Luigi P. Cordella, Pasquale Foggia, Carlo Sansone, Mario Vento;
 ///   *A (Sub)Graph Isomorphism Algorithm for Matching Large Graphs*
-pub fn is_isomorphic<N, E, Ty, Ix>(g0: &Graph<N, E, Ty, Ix>, g1: &Graph<N, E, Ty, Ix>) -> bool
+pub fn is_isomorphic<G0, G1>(g0: &G0, g1: &G1) -> bool
 where
-    Ty: EdgeType,
-    Ix: IndexType,
+    G0: NodeCompactIndexable + EdgeCount + Data,
+    G1: NodeCompactIndexable + EdgeCount + Data,
 {
     if g0.node_count() != g1.node_count() || g0.edge_count() != g1.edge_count() {
         return false;
     }
 
-    let mut st = [Vf2State::new(g0), Vf2State::new(g1)];
-    try_match(&mut st, g0, g1, &mut NoSemanticMatch, &mut NoSemanticMatch).unwrap_or(false)
+    let mut st = Vf2State::<'_, G0, G1::NodeId>::new(g0);
+
+    // let mut st = [Vf2State::new(g0), Vf2State::new(g1)];
+    // try_match(&mut st, g0, g1, &mut NoSemanticMatch, &mut NoSemanticMatch).unwrap_or(false)
+    unimplemented!()
 }
 
 /// [Graph] Return `true` if the graphs `g0` and `g1` are isomorphic.
@@ -178,17 +175,17 @@ where
 /// graph isomorphism (graph structure and matching node and edge weights).
 ///
 /// The graphs should not be multigraphs.
-pub fn is_isomorphic_matching<N, E, Ty, Ix, F, G>(
-    g0: &Graph<N, E, Ty, Ix>,
-    g1: &Graph<N, E, Ty, Ix>,
-    mut node_match: F,
-    mut edge_match: G,
+pub fn is_isomorphic_matching<G0, G1, NM, EM>(
+    g0: &G0,
+    g1: &G1,
+    mut node_match: NM,
+    mut edge_match: EM,
 ) -> bool
 where
-    Ty: EdgeType,
-    Ix: IndexType,
-    F: FnMut(&N, &N) -> bool,
-    G: FnMut(&E, &E) -> bool,
+    G0: NodeCompactIndexable + EdgeCount + Data,
+    G1: NodeCompactIndexable + EdgeCount + Data,
+    NM: FnMut(&G0::NodeWeight, &G1::NodeWeight) -> bool,
+    EM: FnMut(&G0::EdgeWeight, &G1::EdgeWeight) -> bool,
 {
     if g0.node_count() != g1.node_count() || g0.edge_count() != g1.edge_count() {
         return false;
@@ -231,23 +228,24 @@ where
 }
 
 /// Return Some(bool) if isomorphism is decided, else None.
-fn try_match<N, E, Ty, Ix, F, G>(
-    mut st: &mut [Vf2State<Ty, Ix>; 2],
-    g0: &Graph<N, E, Ty, Ix>,
-    g1: &Graph<N, E, Ty, Ix>,
-    node_match: &mut F,
-    edge_match: &mut G,
+fn try_match<G0, G1, NM, EM>(
+    // mut st: &mut [Vf2State<Ty, Ix>; 2],
+    mut st: (Vf2State<'_, G0>, Vf2State<'_, G1>),
+    g0: &G0,
+    g1: &G1,
+    node_match: &mut NM,
+    edge_match: &mut EM,
 ) -> Option<bool>
 where
-    Ty: EdgeType,
-    Ix: IndexType,
-    F: SemanticMatcher<N, N>,
-    G: SemanticMatcher<E, E>,
+    G0: NodeCompactIndexable + EdgeCount + Data,
+    G1: NodeCompactIndexable + EdgeCount + Data,
+    NM: SemanticMatcher<G0::NodeWeight, G1::NodeWeight>,
+    EM: SemanticMatcher<G0::EdgeWeight, G1::EdgeWeight>,
 {
     if st[0].is_complete() {
         return Some(true);
     }
-    let g = [g0, g1];
+    let g = (g0, g1);
     let graph_indices = 0..2;
     let end = NodeIndex::end();
 
@@ -270,8 +268,7 @@ where
         Unwind { nodes: [N; 2], open_list: OpenList },
     }
 
-    let next_candidate =
-        |st: &mut [Vf2State<Ty, Ix>; 2]| -> Option<(NodeIndex<Ix>, NodeIndex<Ix>, OpenList)> {
+    let next_candidate = |st: &mut [Vf2State<Ty, Ix>; 2]| -> Option<(NodeIndex<Ix>, NodeIndex<Ix>, OpenList)> {
             let mut to_index;
             let mut from_index = None;
             let mut open_list = OpenList::Out;
@@ -305,10 +302,7 @@ where
                 _ => None,
             }
         };
-    let next_from_ix = |st: &mut [Vf2State<Ty, Ix>; 2],
-                        nx: NodeIndex<Ix>,
-                        open_list: OpenList|
-     -> Option<NodeIndex<Ix>> {
+    let next_from_ix = |st: &mut [Vf2State<Ty, Ix>; 2], nx: NodeIndex<Ix>, open_list: OpenList| -> Option<NodeIndex<Ix>> {
         // Find the next node index to try on the `from` side of the mapping
         let start = nx.index() + 1;
         let cand0 = match open_list {
