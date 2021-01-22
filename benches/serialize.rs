e7097c6e2682f079c081084745413a6a6fb8aa86
Sebastian K <s3bk@protonmail.com>
2021-01-22 12:03:04 +0300

    improve graph generation

--- benches/serialize.rs
+++ benches/serialize.rs
@@ -5,18 +5,31 @@ extern crate test;
 
 use test::Bencher;
 use petgraph::prelude::*;
+use rand::{Rng};
 
-fn make_stable_graph() -> StableGraph<usize, usize> {
-    let mut g = StableGraph::default();
-    let indices: Vec<_> = (0 .. 10000).map(|i| g.add_node(i)).collect();
-    for i in 0 .. 1000 {
-        g.extend_with_edges((1 .. 1000).map(|j| (indices[j], indices[(j + i) % 10000], i)));
-    }
+const NUM_NODES: usize = 1_000_000;
+const NUM_EDGES: usize = 100_000;
+const NUM_HOLES: usize = 1_000_000;
+
+fn make_stable_graph() -> StableGraph<u32, u32> {
+    let mut g = StableGraph::with_capacity(NUM_NODES + NUM_HOLES, NUM_EDGES);
+    let indices: Vec<_> = (0 .. NUM_NODES + NUM_HOLES).map(|i| g.add_node(i as u32)).collect();
+
+    let mut rng = rand::thread_rng();
+    g.extend_with_edges((0 .. NUM_EDGES).map(|_| {
+        let first = rng.gen_range(0, NUM_NODES + NUM_HOLES);
+        let second = rng.gen_range(0, NUM_NODES + NUM_HOLES - 1);
+        let second = second + (second >= first) as usize;
+        let weight: u32 = rng.gen();
+        (indices[first], indices[second], weight)
+    }));
 
     // Remove nodes to make the structure a bit more interesting
-    for i in (0 .. 10000).step_by(2) {
-        g.remove_node(indices[i]);
+    while g.node_count() > NUM_NODES {
+        let idx = rng.gen_range(0, indices.len());
+        g.remove_node(indices[idx]);
     }
+
     g
 }
 
@@ -34,7 +47,7 @@ fn deserialize_bench(bench: &mut Bencher) {
     let graph = make_stable_graph();
     let data = bincode::serialize(&graph).unwrap();
     bench.iter(|| {
-        let graph2: StableGraph<usize, usize> = bincode::deserialize(&data).unwrap();
+        let graph2: StableGraph<u32, u32> = bincode::deserialize(&data).unwrap();
         graph2
     });
 }
