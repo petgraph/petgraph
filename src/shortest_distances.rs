ec5bdc2e4ee38091da1686016aebd59d351a272a
PrototypeRailGun <52821033+PrototypeRailGun@users.noreply.github.com>
2021-01-25 20:16:32 +0300

    Changed the code style in tests

--- src/shortest_distances.rs
+++ src/shortest_distances.rs
@@ -57,72 +57,146 @@ mod tests {
         let inf = 1.0/0.0;
         
         let mut graph = Graph::<u8, ()>::new();
-        let n0 = graph.add_node(0); let n1 = graph.add_node(1);
-        let n2 = graph.add_node(2); let n3 = graph.add_node(3);
-        let n4 = graph.add_node(4); let n5 = graph.add_node(5);
-        let n6 = graph.add_node(6); let n7 = graph.add_node(7);
-        let n8 = graph.add_node(8); let n9 = graph.add_node(9);
-        let n10 = graph.add_node(10); let n11 = graph.add_node(11);
+        let n0 = graph.add_node(0);
+        let n1 = graph.add_node(1);
+        let n2 = graph.add_node(2); 
+        let n3 = graph.add_node(3);
+        let n4 = graph.add_node(4); 
+        let n5 = graph.add_node(5);
+        let n6 = graph.add_node(6); 
+        let n7 = graph.add_node(7);
+        let n8 = graph.add_node(8); 
+        let n9 = graph.add_node(9);
+        let n10 = graph.add_node(10); 
+        let n11 = graph.add_node(11);
 
-        graph.add_edge(n0, n1, ()); graph.add_edge(n0, n2, ());
-        graph.add_edge(n2, n3, ()); graph.add_edge(n2, n5, ());
-        graph.add_edge(n3, n4, ()); graph.add_edge(n4, n8, ());
-        graph.add_edge(n5, n9, ()); graph.add_edge(n5, n6, ()); 
-        graph.add_edge(n6, n3, ()); graph.add_edge(n6, n7, ());
-        graph.add_edge(n6, n10, ()); graph.add_edge(n7, n8, ());
-        graph.add_edge(n7, n11, ()); graph.add_edge(n8, n11, ()); 
-        graph.add_edge(n9, n1, ()); graph.add_edge(n9, n10, ());
-        graph.add_edge(n10, n6, ()); graph.add_edge(n11, n6, ()); 
-        graph.add_edge(n11, n10, ()); graph.add_edge(n0, n9, ());
+        graph.add_edge(n0, n1, ()); 
+        graph.add_edge(n0, n2, ());
+        graph.add_edge(n2, n3, ()); 
+        graph.add_edge(n2, n5, ());
+        graph.add_edge(n3, n4, ()); 
+        graph.add_edge(n4, n8, ());
+        graph.add_edge(n5, n9, ()); 
+        graph.add_edge(n5, n6, ()); 
+        graph.add_edge(n6, n3, ()); 
+        graph.add_edge(n6, n7, ());
+        graph.add_edge(n6, n10, ()); 
+        graph.add_edge(n7, n8, ());
+        graph.add_edge(n7, n11, ()); 
+        graph.add_edge(n8, n11, ()); 
+        graph.add_edge(n9, n1, ()); 
+        graph.add_edge(n9, n10, ());
+        graph.add_edge(n10, n6, ()); 
+        graph.add_edge(n11, n6, ()); 
+        graph.add_edge(n11, n10, ()); 
+        graph.add_edge(n0, n9, ());
 
-        assert_eq!(shortest_distances(&graph, graph.from_index(0)), 
-                   vec![0.0, 1.0, 1.0, 2.0, 3.0, 2.0, 3.0, 4.0, 4.0, 1.0, 2.0, 5.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(1)), 
-                   vec![inf, 0.0, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(2)), 
-                   vec![inf, 3.0, 0.0, 1.0, 2.0, 1.0, 2.0, 3.0, 3.0, 2.0, 3.0, 4.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(3)), 
-                   vec![inf, inf, inf, 0.0, 1.0, inf, 4.0, 5.0, 2.0, inf, 4.0, 3.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(4)), 
-                   vec![inf, inf, inf, 4.0, 0.0, inf, 3.0, 4.0, 1.0, inf, 3.0, 2.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(5)), 
-                   vec![inf, 2.0, inf, 2.0, 3.0, 0.0, 1.0, 2.0, 3.0, 1.0, 2.0, 3.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(6)), 
-                   vec![inf, inf, inf, 1.0, 2.0, inf, 0.0, 1.0, 2.0, inf, 1.0, 2.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(7)), 
-                   vec![inf, inf, inf, 3.0, 4.0, inf, 2.0, 0.0, 1.0, inf, 2.0, 1.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(8)), 
-                   vec![inf, inf, inf, 3.0, 4.0, inf, 2.0, 3.0, 0.0, inf, 2.0, 1.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(9)), 
-                   vec![inf, 1.0, inf, 3.0, 4.0, inf, 2.0, 3.0, 4.0, 0.0, 1.0, 4.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(10)), 
-                   vec![inf, inf, inf, 2.0, 3.0, inf, 1.0, 2.0, 3.0, inf, 0.0, 3.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(11)), 
-                   vec![inf, inf, inf, 2.0, 3.0, inf, 1.0, 2.0, 3.0, inf, 1.0, 0.0]);
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(0)), 
+            vec![0.0, 1.0, 1.0, 2.0, 3.0, 2.0, 3.0, 4.0, 4.0, 1.0, 2.0, 5.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(1)), 
+            vec![inf, 0.0, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(2)), 
+            vec![inf, 3.0, 0.0, 1.0, 2.0, 1.0, 2.0, 3.0, 3.0, 2.0, 3.0, 4.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(3)), 
+            vec![inf, inf, inf, 0.0, 1.0, inf, 4.0, 5.0, 2.0, inf, 4.0, 3.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(4)), 
+            vec![inf, inf, inf, 4.0, 0.0, inf, 3.0, 4.0, 1.0, inf, 3.0, 2.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(5)), 
+             vec![inf, 2.0, inf, 2.0, 3.0, 0.0, 1.0, 2.0, 3.0, 1.0, 2.0, 3.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(6)), 
+            vec![inf, inf, inf, 1.0, 2.0, inf, 0.0, 1.0, 2.0, inf, 1.0, 2.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(7)), 
+            vec![inf, inf, inf, 3.0, 4.0, inf, 2.0, 0.0, 1.0, inf, 2.0, 1.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(8)), 
+            vec![inf, inf, inf, 3.0, 4.0, inf, 2.0, 3.0, 0.0, inf, 2.0, 1.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(9)), 
+            vec![inf, 1.0, inf, 3.0, 4.0, inf, 2.0, 3.0, 4.0, 0.0, 1.0, 4.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(10)), 
+            vec![inf, inf, inf, 2.0, 3.0, inf, 1.0, 2.0, 3.0, inf, 0.0, 3.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(11)), 
+            vec![inf, inf, inf, 2.0, 3.0, inf, 1.0, 2.0, 3.0, inf, 1.0, 0.0]
+        );
                 
         let mut graph = Graph::<u8, ()>::new();
-        let n0 = graph.add_node(0); let n1 = graph.add_node(1);
-        let n2 = graph.add_node(2); let n3 = graph.add_node(3);
-        let n4 = graph.add_node(4); let n5 = graph.add_node(5);
+        let n0 = graph.add_node(0); 
+        let n1 = graph.add_node(1);
+        let n2 = graph.add_node(2); 
+        let n3 = graph.add_node(3);
+        let n4 = graph.add_node(4); 
+        let n5 = graph.add_node(5);
         let n6 = graph.add_node(6);
 
-        graph.add_edge(n0, n6, ()); graph.add_edge(n0, n1, ());
-        graph.add_edge(n1, n0, ()); graph.add_edge(n1, n2, ());
-        graph.add_edge(n1, n5, ()); graph.add_edge(n1, n6, ());
-        graph.add_edge(n2, n1, ()); graph.add_edge(n2, n3, ());
-        graph.add_edge(n3, n2, ()); graph.add_edge(n3, n4, ());
-        graph.add_edge(n4, n3, ()); graph.add_edge(n4, n5, ());
-        graph.add_edge(n5, n2, ()); graph.add_edge(n5, n6, ());
-        graph.add_edge(n5, n1, ()); graph.add_edge(n5, n4, ());
-        graph.add_edge(n6, n0, ()); graph.add_edge(n6, n1, ());
-        graph.add_edge(n6, n5, ()); graph.add_edge(n2, n5, ()); 
+        graph.add_edge(n0, n6, ()); 
+        graph.add_edge(n0, n1, ());
+        graph.add_edge(n1, n0, ()); 
+        graph.add_edge(n1, n2, ());
+        graph.add_edge(n1, n5, ()); 
+        graph.add_edge(n1, n6, ());
+        graph.add_edge(n2, n1, ()); 
+        graph.add_edge(n2, n3, ());
+        graph.add_edge(n3, n2, ()); 
+        graph.add_edge(n3, n4, ());
+        graph.add_edge(n4, n3, ()); 
+        graph.add_edge(n4, n5, ());
+        graph.add_edge(n5, n2, ()); 
+        graph.add_edge(n5, n6, ());
+        graph.add_edge(n5, n1, ()); 
+        graph.add_edge(n5, n4, ());
+        graph.add_edge(n6, n0, ()); 
+        graph.add_edge(n6, n1, ());
+        graph.add_edge(n6, n5, ()); 
+        graph.add_edge(n2, n5, ()); 
 
-        assert_eq!(shortest_distances(&graph, graph.from_index(0)), vec![0.0, 1.0, 2.0, 3.0, 3.0, 2.0, 1.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(1)), vec![1.0, 0.0, 1.0, 2.0, 2.0, 1.0, 1.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(2)), vec![2.0, 1.0, 0.0, 1.0, 2.0, 1.0, 2.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(3)), vec![3.0, 2.0, 1.0, 0.0, 1.0, 2.0, 3.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(4)), vec![3.0, 2.0, 2.0, 1.0, 0.0, 1.0, 2.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(5)), vec![2.0, 1.0, 1.0, 2.0, 1.0, 0.0, 1.0]);
-        assert_eq!(shortest_distances(&graph, graph.from_index(6)), vec![1.0, 1.0, 2.0, 3.0, 2.0, 1.0, 0.0]);
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(0)), 
+            vec![0.0, 1.0, 2.0, 3.0, 3.0, 2.0, 1.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(1)), 
+            vec![1.0, 0.0, 1.0, 2.0, 2.0, 1.0, 1.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(2)), 
+            vec![2.0, 1.0, 0.0, 1.0, 2.0, 1.0, 2.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(3)), 
+            vec![3.0, 2.0, 1.0, 0.0, 1.0, 2.0, 3.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(4)), 
+            vec![3.0, 2.0, 2.0, 1.0, 0.0, 1.0, 2.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(5)), 
+            vec![2.0, 1.0, 1.0, 2.0, 1.0, 0.0, 1.0]
+        );
+        assert_eq!(
+            shortest_distances(&graph, graph.from_index(6)), 
+            vec![1.0, 1.0, 2.0, 3.0, 2.0, 1.0, 0.0]
+        );
     }
 }
