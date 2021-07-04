44142c7341afd4ed6c89f76aa546a6dcf4dbec67
Agustín Borgna <agustinborgna@gmail.com>
2021-07-04 15:24:24 +0200

    Fix bare url in doc (#450)

--- src/algo/simple_paths.rs
+++ src/algo/simple_paths.rs
@@ -13,7 +13,7 @@ use crate::{
 /// Returns an iterator that produces all simple paths from `from` node to `to`, which contains at least `min_intermediate_nodes` nodes
 /// and at most `max_intermediate_nodes`, if given, or limited by the graph's order otherwise. The simple path is a path without repetitions.
 ///
-/// This algorithm is adapted from https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.simple_paths.all_simple_paths.html
+/// This algorithm is adapted from <https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.simple_paths.all_simple_paths.html>.
 ///
 /// # Example
 /// ```
