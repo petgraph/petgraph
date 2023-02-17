53e3042fd500f8d92d356736515c2f0553ee677d
David Sanders <insideoutclub@gmail.com>
2023-02-17 15:41:32 -0800

    Update matrix_graph.rs

--- src/matrix_graph.rs
+++ src/matrix_graph.rs
@@ -1133,12 +1133,12 @@ impl<N, E, Ty: EdgeType, Null: Nullable<Wrapped = E>, Ix: IndexType> Visitable
     type Map = FixedBitSet;
 
     fn visit_map(&self) -> FixedBitSet {
-        FixedBitSet::with_capacity(self.node_count())
+        FixedBitSet::with_capacity(self.node_bound())
     }
 
     fn reset_map(&self, map: &mut Self::Map) {
         map.clear();
-        map.grow(self.node_count());
+        map.grow(self.node_bound());
     }
 }
 
@@ -1235,7 +1235,7 @@ impl<N, E, Ty: EdgeType, Null: Nullable<Wrapped = E>, Ix: IndexType> NodeIndexab
     for MatrixGraph<N, E, Ty, Null, Ix>
 {
     fn node_bound(&self) -> usize {
-        self.node_count()
+        self.nodes.upper_bound
     }
 
     fn to_index(&self, ix: NodeIndex<Ix>) -> usize {
@@ -1782,5 +1782,38 @@ mod tests {
 
         assert!(!g.has_edge(a, b));
         assert_eq!(g.edge_count(), 0);
+    }
+    #[test]
+    // From https://github.com/petgraph/petgraph/issues/523
+    fn test_tarjan_scc_with_removed_node() {
+        let mut g: MatrixGraph<(), ()> = MatrixGraph::new();
+
+        g.add_node(());
+        let b = g.add_node(());
+        g.add_node(());
+
+        g.remove_node(b);
+
+        assert_eq!(
+            crate::algo::tarjan_scc(&g),
+            [[node_index(0)], [node_index(2)]]
+        );
+    }
+
+    #[test]
+    // From https://github.com/petgraph/petgraph/issues/523
+    fn test_kosaraju_scc_with_removed_node() {
+        let mut g: MatrixGraph<(), ()> = MatrixGraph::new();
+
+        g.add_node(());
+        let b = g.add_node(());
+        g.add_node(());
+
+        g.remove_node(b);
+
+        assert_eq!(
+            crate::algo::kosaraju_scc(&g),
+            [[node_index(2)], [node_index(0)]]
+        );
     }
 }
