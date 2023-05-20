ee4605f629c1fa9c1046d7434184bff20bc29b91
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-05-20 21:22:04 +0200

    docs: test quirks

--- crates/graph/tests/test_undirected.rs
+++ crates/graph/tests/test_undirected.rs
@@ -75,8 +75,8 @@ where
 }
 
 /// Graph: a = b - c
-struct GraphDoubleLink {
-    graph: Graph<(), (), Undirected>,
+struct GraphDoubleLink<N = (), E = ()> {
+    graph: Graph<N, E, Undirected>,
     a: NodeIndex,
     b: NodeIndex,
     c: NodeIndex,
@@ -86,17 +86,21 @@ struct GraphDoubleLink {
     bc: EdgeIndex,
 }
 
-impl GraphDoubleLink {
-    fn new() -> Self {
+impl<N, E> GraphDoubleLink<N, E>
+where
+    N: Default,
+    E: Default,
+{
+    fn new_from_default() -> Self {
         let mut graph = Graph::new_undirected();
 
-        let a = graph.add_node(());
-        let b = graph.add_node(());
-        let c = graph.add_node(());
+        let a = graph.add_node(N::default());
+        let b = graph.add_node(N::default());
+        let c = graph.add_node(N::default());
 
-        let ab = graph.add_edge(a, b, ());
-        let ba = graph.add_edge(b, a, ());
-        let bc = graph.add_edge(b, c, ());
+        let ab = graph.add_edge(a, b, E::default());
+        let ba = graph.add_edge(b, a, E::default());
+        let bc = graph.add_edge(b, c, E::default());
 
         Self {
             graph,
@@ -107,6 +111,12 @@ impl GraphDoubleLink {
             ba,
             bc,
         }
+    }
+}
+
+impl GraphDoubleLink {
+    fn new() -> Self {
+        Self::new_from_default()
     }
 }
 
@@ -219,7 +229,7 @@ fn neighbours() {
     assert_eq!(graph.neighbors(c).count(), 1);
 
     assert_eq!(graph.neighbors(a).collect::<Vec<_>>(), vec![b, b]);
-    assert_eq!(graph.neighbors(b).collect::<Vec<_>>(), vec![a, a, c]);
+    assert_eq!(graph.neighbors(b).collect::<Vec<_>>(), vec![c, a, a]);
     assert_eq!(graph.neighbors(c).collect::<Vec<_>>(), vec![b]);
 }
 
@@ -228,13 +238,21 @@ fn neighbours() {
 fn neighbours_after_removal() {
     let GraphDoubleLink {
         mut graph, a, b, c, ..
-    } = GraphDoubleLink::new();
+    } = GraphDoubleLink::<u32, ()>::new_from_default();
+
+    // mark note c, we need this later when we remove, to ensure the swap was correct
+    graph[c] = 2;
 
     assert_eq!(graph.neighbors(a).count(), 2);
     assert_eq!(graph.neighbors(b).count(), 3);
     assert_eq!(graph.neighbors(c).count(), 1);
 
     graph.remove_node(a);
+
+    // The last index is switched with the first index, therefore we need to rebind
+    let c = a;
+    // ensure that we still point to `c`
+    assert_eq!(graph[c], 2);
 
     assert_eq!(graph.neighbors(b).collect::<Vec<_>>(), vec![c]);
     assert_eq!(graph.neighbors(c).collect::<Vec<_>>(), vec![b]);
@@ -294,12 +312,12 @@ fn neighbours_directed_equivalent() {
 #[test]
 fn multiple() {
     let GraphDoubleSameDirection {
-        graph, a, b, ab1, ..
+        graph, a, b, ab2, ..
     } = GraphDoubleSameDirection::new();
 
     assert_eq!(graph.neighbors(a).count(), 2);
     assert_eq!(graph.neighbors(b).count(), 2);
-    assert_eq!(graph.find_edge(a, b), Some(ab1));
+    assert_eq!(graph.find_edge(a, b), Some(ab2));
     assert_eq!(graph.edge_count(), 2);
 
     assert_graph_consistency(&graph);
@@ -400,11 +418,23 @@ fn access_removed_node() {
         ab,
     } = GraphLink::<u32, ()>::new();
 
+    // mark b with a weight of 1
+    // we will check this later
+    graph[b] = 1;
+
     graph.remove_node(a);
 
+    // This is a bit unintuitive, but to preserve indices, the last index is swapped with the
+    // first index. Therefore we actually have to check for b here.
+
+    // To verify that `b` is actually at the `a` index now, we check the weight, we previously set.
+    // Remember: `a` would have a weight of 0, because we never set it.
+    assert_eq!(graph[a], 1);
+
     let result = std::panic::catch_unwind(|| {
+        // now we access `b`, which should panic because we swapped it with `a`, when we removed it.
+        let access = graph[b];
         // ensure that our access is not optimized away
-        let access = graph[a];
         core::hint::black_box(&access);
     });
 
