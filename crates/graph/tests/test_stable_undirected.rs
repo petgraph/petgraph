8d360b7593c8a6c88e3f2e67cb961635996e41db
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-06-01 12:38:15 +0200

    test: stable graph

--- crates/graph/tests/test_stable_undirected.rs
+++ crates/graph/tests/test_stable_undirected.rs
@@ -6,7 +6,7 @@ use petgraph_core::{
     index::DefaultIx,
     visit::{EdgeIndexable, EdgeRef, IntoEdgeReferences, IntoNodeReferences, NodeIndexable},
 };
-use petgraph_graph::{stable::StableGraph, EdgeIndex, Graph, NodeIndex};
+use petgraph_graph::{stable::StableGraph, Graph, NodeIndex};
 
 use crate::common::graphs::GraphDoubleLink;
 
@@ -242,6 +242,37 @@ fn edges_multi() {
 }
 
 #[test]
+fn edges_multi_connecting() {
+    let GraphDoubleSameDirection {
+        graph,
+        a,
+        b,
+        ab1,
+        ab2,
+    } = GraphDoubleSameDirection::new();
+
+    assert_eq!(graph.edges_connecting(a, b).count(), 2);
+
+    assert_eq!(
+        graph
+            .edges_connecting(a, b)
+            .map(|reference| reference.id())
+            .collect::<Vec<_>>(),
+        vec![ab2, ab1]
+    );
+
+    assert_eq!(graph.edges_connecting(b, a).count(), 2);
+
+    assert_eq!(
+        graph
+            .edges_connecting(b, a)
+            .map(|reference| reference.id())
+            .collect::<Vec<_>>(),
+        vec![ab2, ab1]
+    );
+}
+
+#[test]
 fn edges_directed() {
     let GraphLink { graph, a, b, ab } = GraphLink::new();
 
@@ -280,6 +311,113 @@ fn edges_directed() {
             .collect::<Vec<_>>(),
         vec![ab]
     );
+}
+
+#[test]
+fn neighbours() {
+    let GraphLink { graph, a, b, .. } = GraphLink::new();
+
+    assert_eq!(graph.neighbors(a).count(), 1);
+    assert_eq!(graph.neighbors(b).count(), 1);
+
+    assert_eq!(graph.neighbors(a).collect::<Vec<_>>(), vec![b]);
+    assert_eq!(graph.neighbors(b).collect::<Vec<_>>(), vec![a]);
+}
+
+#[test]
+fn neighbours_directed() {
+    let GraphLink { graph, a, b, .. } = GraphLink::new();
+
+    assert_eq!(graph.neighbors_directed(a, Direction::Outgoing).count(), 1);
+    assert_eq!(graph.neighbors_directed(a, Direction::Incoming).count(), 1);
+
+    assert_eq!(graph.neighbors_directed(b, Direction::Outgoing).count(), 1);
+    assert_eq!(graph.neighbors_directed(b, Direction::Incoming).count(), 1);
+
+    assert_eq!(
+        graph
+            .neighbors_directed(a, Direction::Outgoing)
+            .collect::<Vec<_>>(),
+        vec![b]
+    );
+    assert_eq!(
+        graph
+            .neighbors_directed(a, Direction::Incoming)
+            .collect::<Vec<_>>(),
+        vec![b]
+    );
+
+    assert_eq!(
+        graph
+            .neighbors_directed(b, Direction::Outgoing)
+            .collect::<Vec<_>>(),
+        vec![a]
+    );
+    assert_eq!(
+        graph
+            .neighbors_directed(b, Direction::Incoming)
+            .collect::<Vec<_>>(),
+        vec![a]
+    );
+}
+
+#[test]
+fn node_references() {
+    let GraphLink { graph, a, b, .. } = GraphLink::new();
+
+    assert_eq!(graph.node_references().count(), 2);
+
+    assert_eq!(
+        graph
+            .node_references()
+            .map(|(index, _)| index)
+            .collect::<Vec<_>>(),
+        vec![a, b]
+    );
+}
+
+#[test]
+fn edge_references() {
+    let GraphLink { graph, a, b, ab } = GraphLink::new();
+
+    assert_eq!(graph.edge_references().count(), 1);
+
+    assert_eq!(
+        graph
+            .edge_references()
+            .map(|edge| (edge.source(), edge.target(), edge.id()))
+            .collect::<Vec<_>>(),
+        vec![(a, b, ab)]
+    );
+}
+
+#[test]
+fn nodes_mut() {
+    let GraphLink {
+        mut graph, a, b, ..
+    } = GraphLink::<i32, ()>::from_default();
+
+    assert_eq!(graph.node_weights_mut().count(), 2);
+
+    for (index, weight) in graph.node_weights_mut().enumerate() {
+        *weight = i32::try_from(index).expect("should be able to convert") + 1;
+    }
+
+    assert_eq!(graph.node_weight(a), Some(&1));
+    assert_eq!(graph.node_weight(b), Some(&2));
+}
+
+#[test]
+fn edges_mut() {
+    let GraphLink { mut graph, ab, .. } = GraphLink::<(), i32>::from_default();
+
+    assert_eq!(graph.edge_weights_mut().count(), 1);
+
+    for (index, weight) in graph.edge_weights_mut().enumerate() {
+        *weight = i32::try_from(index).expect("should be able to convert") + 1;
+    }
+
+    assert_eq!(graph.edge_weight(ab), Some(&1));
 }
 
 #[cfg(feature = "std")]
