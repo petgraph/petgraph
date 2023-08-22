2549f92e9ad900e4b301154cfb47f69a8ae63ed4
Daniel Levin <daniellevin2607@gmail.com>
2023-08-22 09:24:27 +0200

    Did more work in each benchmark.

--- benches/graphmap.rs
+++ benches/graphmap.rs
@@ -17,7 +17,7 @@ struct MyStruct {
 
 fn test_nodes() -> Vec<MyStruct> {
     let mut nodes = vec![];
-    for i in 0..25_000 {
+    for i in 0..2500 {
         nodes.push(MyStruct {
             u: format!("X {}", i),
             v: format!("Y {} Y", i),
@@ -31,7 +31,7 @@ fn test_nodes() -> Vec<MyStruct> {
 fn test_graph(data: &Vec<MyStruct>) -> DiGraphMap<&MyStruct, usize> {
     let mut gr: DiGraphMap<&MyStruct, usize> = DiGraphMap::new();
 
-    for i in 0..25_000 {
+    for i in 0..2500 {
         gr.add_node(&data[i]);
     }
 
@@ -49,8 +49,13 @@ fn graphmap_serial_bench(bench: &mut Bencher) {
     let data = test_nodes();
     let gr = test_graph(&data);
     bench.iter(|| {
+        let mut sources = vec![];
         for n in gr.nodes() {
-            gr.edges_directed(n, Direction::Outgoing);
+            for (src, _, e) in gr.edges_directed(n, Direction::Outgoing) {
+                if *e == 500 {
+                    sources.push(src.clone());
+                }
+            }
         }
     });
 }
@@ -60,8 +65,15 @@ fn graphmap_parallel_bench(bench: &mut Bencher) {
     let data = test_nodes();
     let gr = test_graph(&data);
     bench.iter(|| {
-        gr.par_nodes().for_each(|n| {
-            gr.edges_directed(n, Direction::Outgoing);
-        });
+        let sources: Vec<MyStruct> = gr.par_nodes().map(|n| {
+            let mut sources = vec![];
+            for (src, _, e) in gr.edges_directed(n, Direction::Outgoing) {
+                if *e == 500 {
+                    sources.push(src.clone());
+                }
+            }
+
+            sources
+        }).flatten().collect();
     });
 }
