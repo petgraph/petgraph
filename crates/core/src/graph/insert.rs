f4f3dc5c0dd52d34349ce9276ccb54d8e05e626b
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-10-13 14:17:25 +0200

    feat: docs insert

--- crates/core/src/graph/insert.rs
+++ crates/core/src/graph/insert.rs
@@ -88,7 +88,7 @@ where
         attributes: impl Into<Attributes<<S::NodeId as GraphId>::AttributeIndex, S::NodeWeight>>,
     ) -> NodeMut<S> {
         self.try_insert_node(attributes)
-            .expect("Invariant violation. Try using `try_insert_node` instead.")
+            .expect("Constraint violation. Try using `try_insert_node` instead.")
     }
 }
 
@@ -175,7 +175,7 @@ where
         weight: impl FnOnce(&S::NodeId) -> S::NodeWeight,
     ) -> NodeMut<S> {
         self.try_insert_node_with(weight)
-            .expect("Invariant violation. Try using `try_insert_node_with` instead.")
+            .expect("Constraint violation. Try using `try_insert_node_with` instead.")
     }
 }
 
@@ -222,14 +222,14 @@ where
     /// Insert a node, or update the weight of an existing node.
     ///
     /// This is the infallible version of [`Self::try_upsert_node`], which will panic instead.
-    ///
+    // TODO: Example
     /// # Panics
     ///
     /// The same panics as [`Self::try_upsert_node`] may occur, as well as the ones from
     /// [`Self::insert_node`].
     pub fn upsert_node(&mut self, id: S::NodeId, weight: S::NodeWeight) -> NodeMut<S> {
         self.try_upsert_node(id, weight)
-            .expect("unable to insert node. Try using `try_upsert_node` instead.")
+            .expect("Constraint violation. Try using `try_upsert_node` instead.")
     }
 }
 
@@ -237,6 +237,30 @@ impl<S> Graph<S>
 where
     S: GraphStorage,
 {
+    /// Try to insert an edge with the given attributes.
+    ///
+    /// This is the fallible version of [`Self::insert_edge`].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use petgraph_dino::DiDinoGraph;
+    ///
+    /// let mut graph = DiDinoGraph::new();
+    /// let a = *graph.insert_node(()).id();
+    /// let b = *graph.insert_node(()).id();
+    ///
+    /// let edge = graph
+    ///     .try_insert_edge((), &a, &b)
+    ///     .expect("unable to insert edge");
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// If any of the constraints that are required for the graph storage to be valid are violated.
+    /// This may include things like parallel edges or self loops depending on implementation.
+    ///
+    /// Refer to the documentation of the underlying storage for more information.
     pub fn try_insert_edge(
         &mut self,
         attributes: impl Into<Attributes<<S::EdgeId as GraphId>::AttributeIndex, S::EdgeWeight>>,
@@ -249,6 +273,32 @@ where
         self.storage.insert_edge(id, weight, source, target)
     }
 
+    /// Insert an edge with the given attributes.
+    ///
+    /// Depending on the storage type, or if your code is generic over the storage type, you might
+    /// want to consider using [`Self::try_insert_edge`] instead.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use petgraph_dino::DiDinoGraph;
+    ///
+    /// let mut graph = DiDinoGraph::new();
+    /// let a = *graph.insert_node(()).id();
+    /// let b = *graph.insert_node(()).id();
+    ///
+    /// let edge = graph.insert_edge((), &a, &b);
+    /// ```
+    ///
+    /// # Rationale
+    ///
+    /// For the rationale behind splitting this into [`Self::insert_edge`] and
+    /// [`Self::try_insert_edge`], see the documentation of [`Self::insert_node`].
+    ///
+    /// # Panics
+    ///
+    /// If any of the constraints that are required for the graph storage to be valid are violated.
+    /// This may include things like parallel edges or self loops depending on implementation.
     pub fn insert_edge(
         &mut self,
         attributes: impl Into<Attributes<<S::EdgeId as GraphId>::AttributeIndex, S::EdgeWeight>>,
@@ -256,7 +306,7 @@ where
         target: &S::NodeId,
     ) -> EdgeMut<S> {
         self.try_insert_edge(attributes, source, target)
-            .expect("unable to insert edge")
+            .expect("Constraint violation. Try using `try_insert_edge` instead.")
     }
 }
 
@@ -265,6 +315,39 @@ where
     S: GraphStorage,
     S::EdgeId: ManagedGraphId,
 {
+    /// Insert an edge, where the weight is dependent on the id.
+    ///
+    /// This is similar to [`Self::try_insert_edge`], but instead of providing the weight as a
+    /// parameter, it will take into account the future id, by calling the given closure with the
+    /// id.
+    ///
+    /// This is the fallible version of [`Self::insert_edge_with`].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use petgraph_dino::{DiDinoGraph, EdgeId};
+    ///
+    /// struct Edge {
+    ///     id: EdgeId,
+    /// }
+    ///
+    /// let mut graph = DiDinoGraph::new();
+    /// let a = *graph.insert_node(()).id();
+    /// let b = *graph.insert_node(()).id();
+    ///
+    /// let edge_id = *graph
+    ///     .try_insert_edge_with(|id| Edge { id: *id }, &a, &b)
+    ///     .expect("unable to insert edge");
+    ///
+    /// let edge = graph.edge(&edge_id).expect("edge must exist");
+    ///
+    /// assert_eq!(edge.weight().id, edge_id);
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// The same errors as [`Self::try_insert_edge`] may occur.
     pub fn try_insert_edge_with(
         &mut self,
         weight: impl FnOnce(&S::EdgeId) -> S::EdgeWeight,
@@ -277,6 +360,37 @@ where
         self.storage.insert_edge(id, weight, source, target)
     }
 
+    /// Insert an edge, where the weight is dependent on the id.
+    ///
+    /// This is similar to [`Self::insert_edge`], but instead of providing the weight as a
+    /// parameter, it will take into account the future id, by calling the given closure with the
+    /// id.
+    ///
+    /// This is the infallible version of [`Self::try_insert_edge_with`], which will panic instead.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use petgraph_dino::{DiDinoGraph, EdgeId};
+    ///
+    /// struct Edge {
+    ///     id: EdgeId,
+    /// }
+    ///
+    /// let mut graph = DiDinoGraph::new();
+    /// let a = *graph.insert_node(()).id();
+    /// let b = *graph.insert_node(()).id();
+    ///
+    /// let edge_id = *graph.insert_edge_with(|id| Edge { id: *id }, &a, &b);
+    ///
+    /// let edge = graph.edge(&edge_id).expect("edge must exist");
+    ///
+    /// assert_eq!(edge.weight().id, edge_id);
+    /// ```
+    ///
+    /// # Panics
+    ///
+    /// The same panics as [`Self::insert_edge`] may occur.
     pub fn insert_edge_with(
         &mut self,
         weight: impl FnOnce(&S::EdgeId) -> S::EdgeWeight,
@@ -284,7 +398,7 @@ where
         target: &S::NodeId,
     ) -> EdgeMut<S> {
         self.try_insert_edge_with(weight, source, target)
-            .expect("unable to insert edge")
+            .expect("Constraint violation. Try using `try_insert_edge_with` instead.")
     }
 }
 
@@ -293,7 +407,23 @@ where
     S: GraphStorage,
     S::EdgeId: ArbitraryGraphId,
 {
-    pub fn upsert_edge(
+    /// Insert an edge, or update the weight of an existing edge, if it exists.
+    ///
+    /// This is the fallible version of [`Self::upsert_edge`].
+    // TODO: Example
+    ///
+    /// # Errors
+    ///
+    /// The same errors as [`Self::try_insert_edge`] may occur.
+    ///
+    /// # Panics
+    ///
+    /// If the storage is inconsistent.
+    /// This should never happen, as this is an implementation error of the underlying
+    /// [`GraphStorage`], which must guarantee that if one calls [`GraphStorage::contains_edge`]
+    /// with the id of the edge to check if an edge exists, and then calls
+    /// [`GraphStorage::edge_mut`] with the same id, it must return an edge.
+    pub fn try_upsert_edge(
         &mut self,
         id: S::EdgeId,
         weight: S::EdgeWeight,
@@ -313,5 +443,25 @@ where
         } else {
             self.storage.insert_edge(id, weight, source, target)
         }
+    }
+
+    /// Insert an edge, or update the weight of an existing edge, if it exists.
+    ///
+    /// This is the infallible version of [`Self::try_upsert_edge`], which will panic instead.
+    ///
+    /// # Panics
+    ///
+    /// The same panics as [`Self::try_upsert_edge`] may occur, as well as the ones from
+    /// [`Self::insert_edge`].
+    pub fn upsert_edge(
+        &mut self,
+        id: S::EdgeId,
+        weight: S::EdgeWeight,
+
+        source: &S::NodeId,
+        target: &S::NodeId,
+    ) -> EdgeMut<S> {
+        self.try_upsert_edge(id, weight, source, target)
+            .expect("Constraint violation. Try using `try_upsert_edge` instead.")
     }
 }
