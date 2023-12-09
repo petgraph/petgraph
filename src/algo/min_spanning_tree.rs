2846f36c8bb7450214f9ff6147c24f7b819e852f
BryanCruz <bryan.bialokur@gmail.com>
2023-12-09 16:06:20 -0300

    add prim algorithm to find mst

--- src/algo/min_spanning_tree.rs
+++ src/algo/min_spanning_tree.rs
@@ -1,13 +1,13 @@
 //! Minimum Spanning Tree algorithms.
 
-use std::collections::{BinaryHeap, HashMap};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
 use crate::prelude::*;
 
 use crate::data::Element;
 use crate::scored::MinScored;
 use crate::unionfind::UnionFind;
-use crate::visit::{Data, IntoNodeReferences, NodeRef};
+use crate::visit::{Data, IntoEdges, IntoNodeReferences, NodeRef};
 use crate::visit::{IntoEdgeReferences, NodeIndexable};
 
 /// \[Generic\] Compute a *minimum spanning tree* of a graph.
@@ -22,6 +22,10 @@ use crate::visit::{IntoEdgeReferences, NodeIndexable};
 /// and **|V| - c** edges, where **c** is the number of connected components in `g`.
 ///
 /// Use `from_elements` to create a graph from the resulting iterator.
+///
+/// See also: [`.min_spanning_tree_prim(g)`][1] for implementation using Prim's algorithm.
+///
+/// [1]: fn.min_spanning_tree_prim.html
 pub fn min_spanning_tree<G>(g: G) -> MinSpanningTree<G>
 where
     G::NodeWeight: Clone,
@@ -115,5 +119,132 @@ where
             }
         }
         None
+    }
+}
+
+/// \[Generic\] Compute a *minimum spanning tree* of a graph using Prim's algorithm.
+///
+/// Behavior is undefined if input graph is disconnected (has more than 1 component)
+/// or if graph is directed.
+///
+/// Using Prim's algorithm with runtime **O(|E| log |E|)**.
+///
+/// The resulting graph has all the vertices of the input graph (with identical node indices),
+/// and **|V| - 1** edges.
+///
+/// Use `from_elements` to create a graph from the resulting iterator.
+///
+/// See also: [`.min_spanning_tree(g)`][1] for implementation using Kruskal's algorithm and support for minimum spanning forest.
+///
+/// [1]: fn.min_spanning_tree.html
+pub fn min_spanning_tree_prim<G>(g: G) -> MinSpanningTreePrim<G>
+where
+    G::NodeWeight: Clone,
+    G::EdgeWeight: Clone + PartialOrd,
+    G: IntoNodeReferences + IntoEdgeReferences + NodeIndexable,
+{
+    let sort_edges = BinaryHeap::with_capacity(g.edge_references().size_hint().0);
+    let nodes_taken = HashSet::with_capacity(g.node_references().size_hint().0);
+    let initial_node = g.node_references().next();
+
+    MinSpanningTreePrim {
+        graph: g,
+        node_ids: Some(g.node_references()),
+        node_map: HashMap::new(),
+        node_count: 0,
+        sort_edges,
+        nodes_taken,
+        initial_node,
+    }
+}
+
+/// An iterator producing a minimum spanning tree of a graph.
+/// It will first iterate all Node elements from original graph,
+/// then iterate Edge elements from computed minimum spanning tree.
+#[derive(Debug, Clone)]
+pub struct MinSpanningTreePrim<G>
+where
+    G: Data + IntoNodeReferences,
+{
+    graph: G,
+    node_ids: Option<G::NodeReferences>,
+    node_map: HashMap<usize, usize>,
+    node_count: usize,
+    sort_edges: BinaryHeap<MinScored<G::EdgeWeight, (G::NodeId, G::NodeId)>>,
+    nodes_taken: HashSet<usize>,
+    initial_node: Option<G::NodeRef>,
+}
+
+impl<G> Iterator for MinSpanningTreePrim<G>
+where
+    G: IntoNodeReferences + IntoEdges + NodeIndexable,
+    G::NodeWeight: Clone,
+    G::EdgeWeight: Clone + PartialOrd,
+{
+    type Item = Element<G::NodeWeight, G::EdgeWeight>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Iterate through Node elements
+        let g = self.graph;
+        if let Some(ref mut iter) = self.node_ids {
+            if let Some(node) = iter.next() {
+                self.node_map.insert(g.to_index(node.id()), self.node_count);
+                self.node_count += 1;
+                return Some(Element::Node {
+                    weight: node.weight().clone(),
+                });
+            }
+        }
+        self.node_ids = None;
+
+        // Bootstrap Prim's algorithm to find MST Edge elements.
+        // Mark initial node as taken and add its edges to priority queue.
+        if let Some(initial_node) = self.initial_node {
+            let initial_node_index = g.to_index(initial_node.id());
+            self.nodes_taken.insert(initial_node_index);
+
+            let initial_edges = g.edges(initial_node.id());
+            for edge in initial_edges {
+                self.sort_edges.push(MinScored(
+                    edge.weight().clone(),
+                    (edge.source(), edge.target()),
+                ));
+            }
+        };
+        self.initial_node = None;
+
+        // Prim's algorithm:
+        // Iterate through Edge elements, adding an edge to the MST iff some of it's nodes are not part of MST yet.
+        while let Some(MinScored(score, (source, target))) = self.sort_edges.pop() {
+            let (source_index, target_index) = (g.to_index(source), g.to_index(target));
+
+            if self.nodes_taken.contains(&target_index) {
+                continue;
+            }
+
+            self.nodes_taken.insert(target_index);
+            for edge in g.edges(target) {
+                self.sort_edges.push(MinScored(
+                    edge.weight().clone(),
+                    (edge.source(), edge.target()),
+                ));
+            }
+
+            let (&source_order, &target_order) = match (
+                self.node_map.get(&source_index),
+                self.node_map.get(&target_index),
+            ) {
+                (Some(source_order), Some(target_order)) => (source_order, target_order),
+                _ => panic!("Edge references unknown node"),
+            };
+
+            return Some(Element::Edge {
+                source: source_order,
+                target: target_order,
+                weight: score,
+            });
+        }
+
+        return None;
     }
 }
