02fc5bc23d467d609ae8f783cb9d6e72677f7743
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-12-24 11:27:42 +0100

    feat(algorithms): documentation for implementation

--- crates/algorithms/src/shortest_paths/bellman_ford/tests.rs
+++ crates/algorithms/src/shortest_paths/bellman_ford/tests.rs
@@ -8,7 +8,7 @@ graph!(
     /// Uses the graph from networkx
     ///
     /// <https://github.com/networkx/networkx/blob/main/networkx/algorithms/shortest_paths/tests/test_weighted.py>
-    factory(networkx) => DiDinoGraph<&'static str, i32>;
+    factory(networkx) => DiDinoGraph<&'static str, f32>;
     [
         a: "A",
         b: "B",
@@ -16,26 +16,22 @@ graph!(
         d: "D",
         e: "E",
     ] as NodeId, [
-        ab: a -> b: 10,
-        ac: a -> c: 5,
-        bd: b -> d: 1,
-        bc: b -> c: 2,
-        de: d -> e: 1,
-        cb: c -> b: 3,
-        cd: c -> d: 5,
-        ce: c -> e: 2,
-        ea: e -> a: 7,
-        ed: e -> d: 6,
+        ab: a -> b: 10f32,
+        ac: a -> c: 5f32,
+        bd: b -> d: 1f32,
+        bc: b -> c: 2f32,
+        de: d -> e: 1f32,
+        cb: c -> b: 3f32,
+        cd: c -> d: 5f32,
+        ce: c -> e: 2f32,
+        ea: e -> a: 7f32,
+        ed: e -> d: 6f32,
     ] as EdgeId
 );
 
 #[test]
 fn source_does_not_exist() {
-    let GraphCollection {
-        mut graph,
-        nodes,
-        edges,
-    } = networkx::create();
+    let GraphCollection { mut graph, .. } = networkx::create();
 
     let f = *graph.insert_node("F").id();
     graph.remove_node(&f);
@@ -47,4 +43,25 @@ fn source_does_not_exist() {
     let error = received.current_context();
 
     assert_eq!(error, &BellmanFordError::NodeNotFound);
+}
+
+#[test]
+fn negative_cycle_heuristic() {
+    let GraphCollection {
+        mut graph, nodes, ..
+    } = networkx::create();
+
+    graph.insert_edge(-1, &nodes.a, &nodes.b);
+    graph.insert_edge(-1, &nodes.b, &nodes.c);
+    graph.insert_edge(-1, &nodes.c, &nodes.d);
+    graph.insert_edge(3, &nodes.d, &nodes.a);
+
+    let spfa = BellmanFord::directed();
+    assert!(spfa.every_path(&graph).is_ok());
+
+    let da = *graph.insert_edge(1.999, &nodes.d, &nodes.a).id();
+    assert!(spfa.every_path(&graph).is_err());
+
+    *graph.edge_mut(&da).unwrap().weight_mut() = 2.0;
+    assert!(spfa.every_path(&graph).is_ok());
 }
--- crates/algorithms/src/shortest_paths/mod.rs
+++ crates/algorithms/src/shortest_paths/mod.rs
@@ -1,3 +1,55 @@
+//! # Shortest Path Module
+//!
+//! This module contains traits and implementations for shortest path and shortest distance
+//! algorithms.
+//! These algorithms are used to find the shortest path or distance between two nodes in a graph.
+//!
+//! ## Traits
+//!
+//! - [`ShortestPath`]: Any implementation of this trait can be used to find the shortest path,
+//!   depending on algorithm some restrictions may apply.
+//! - [`ShortestDistance`]: Any implementation of this trait can be used to find the shortest
+//!   distance, depending on algorithm some restrictions may apply.
+//!
+//! ## Implementations
+//!
+//! These are the algorithms implemented in `petgraph` itself, companion crates may implement other
+//! algorithms.
+//!
+//! - [`AStar`]: An implementation of the A* shortest path algorithm.
+//! - [`BellmanFord`]: An implementation of the Bellman-Ford shortest path algorithm.
+//! - [`Dijkstra`]: An implementation of the Dijkstra's shortest path algorithm.
+//! - [`FloydWarshall`]: An implementation of the Floyd-Warshall shortest path algorithm.
+//!
+//! ## Usage
+//!
+//! Each algorithm provides methods to find the shortest path or distance from a source node to a
+//! target node, from a source node to all other nodes, and between all pairs of nodes.
+//!
+//! The [`ShortestPath`] trait provides methods to find paths, while the [`ShortestDistance`] trait
+//! provides methods to find distances.
+//! The difference between a path and a distance is that a path includes the sequence of nodes and
+//! edges, while a distance is just the sum of the weights of the edges.
+//!
+//! ## Example
+//!
+//! ```rust
+//! use petgraph_algorithms::shortest_paths::{Dijkstra, ShortestPath};
+//! use petgraph_dino::DiDinoGraph;
+//!
+//! let mut graph = DiDinoGraph::new();
+//! let a = *graph.insert_node("A").id();
+//! let b = *graph.insert_node("B").id();
+//! graph.insert_edge(7, &a, &b);
+//!
+//! let dijkstra = Dijkstra::directed();
+//! let path = dijkstra.path_between(&graph, &a, &b);
+//! assert!(path.is_some());
+//!
+//! let path = dijkstra.path_between(&graph, &b, &a);
+//! assert!(path.is_none());
+//! ```
+
 pub mod astar;
 pub mod bellman_ford;
 mod common;
@@ -19,13 +71,35 @@ pub use self::{
     floyd_warshall::FloydWarshall,
 };
 
+/// # Shortest Path
+///
+/// A shortest path algorithm is an algorithm that finds a path between two nodes in a graph such
+/// that the sum of the weights of its constituent edges is minimized.
+///
+/// Different algorithms exist to solve this problem, each with its own trade-offs, refer to the
+/// different algorithms for more information.
+///
+/// ## Shortest Path vs Shortest Distance
+///
+/// The shortest path is the path between two nodes that has the lowest cost.
+/// The shortest distance is the cost of the shortest path.
+/// You should prefer shortest distance algorithms over shortest path algorithms if you are only
+/// interested in the cost of the shortest path.
+// TODO: example?
 pub trait ShortestPath<S>
 where
     S: GraphStorage,
 {
+    /// The cost of the shortest path.
     type Cost;
+
+    /// The error that can occur when computing the shortest path.
     type Error: Context;
 
+    /// Returns the shortest path from the source to the target.
+    ///
+    /// Returns an iterator over all routes in the graph that end at the given target node.
+    // TODO: example?
     fn path_to<'graph: 'this, 'this>(
         &'this self,
         graph: &'graph Graph<S>,
@@ -36,6 +110,11 @@ where
         Ok(iter.filter(move |route| route.path().target().id() == target))
     }
 
+    /// Returns the shortest path from the source to the target.
+    ///
+    /// This is also known as `SSSP` (Single Source Shortest Path) problem.
+    ///
+    /// Returns an iterator over all routes in the graph that start at the given source node.
     fn path_from<'graph: 'this, 'this>(
         &'this self,
         graph: &'graph Graph<S>,
@@ -46,6 +125,9 @@ where
         Ok(iter.filter(move |route| route.path().source().id() == source))
     }
 
+    /// Returns the shortest path from the source to the target.
+    ///
+    /// This will return [`None`] if no path exists between the source and the target.
     fn path_between<'graph>(
         &self,
         graph: &'graph Graph<S>,
@@ -57,19 +139,46 @@ where
             .find(|route| route.path().target().id() == target)
     }
 
+    /// Returns an iterator over all shortest paths in the graph.
+    ///
+    /// This is also known as `APSP` (All Pairs Shortest Path) problem.
+    ///
+    /// Returns an iterator over all routes in the graph.
     fn every_path<'graph: 'this, 'this>(
         &'this self,
         graph: &'graph Graph<S>,
     ) -> Result<impl Iterator<Item = Route<'graph, S, Self::Cost>> + 'this, Self::Error>;
 }
 
+/// # Shortest Distance
+///
+/// A shortest distance algorithm is an algorithm that finds the distance between two nodes in a
+/// graph such that the sum of the weights of its constituent edges is minimized.
+///
+/// Different algorithms exist to solve this problem, each with its own trade-offs, refer to the
+/// different algorithms for more information.
+///
+/// ## Shortest Path vs Shortest Distance
+///
+/// The shortest path is the path between two nodes that has the lowest cost.
+/// The shortest distance is the cost of the shortest path.
+/// You should prefer shortest distance algorithms over shortest path algorithms if you are only
+/// interested in the cost of the shortest path.
+// TODO: example?
 pub trait ShortestDistance<S>
 where
     S: GraphStorage,
 {
+    /// The cost of the shortest path.
     type Cost;
+
+    /// The error that can occur when computing the shortest path.
     type Error: Context;
 
+    /// Returns the shortest distance from the source to the target.
+    ///
+    /// Returns an iterator over all direct routes in the graph that end at the given target node.
+    /// A [`DirectRoute`] does not contain the nodes traversed, only the source and target nodes.
     fn distance_to<'graph: 'this, 'this>(
         &'this self,
         graph: &'graph Graph<S>,
@@ -79,6 +188,13 @@ where
 
         Ok(iter.filter(move |route| route.target().id() == target))
     }
+
+    /// Returns the shortest distance from the source to the target.
+    ///
+    /// This is also known as `SSSP` (Single Source Shortest Path) problem.
+    ///
+    /// Returns an iterator over all direct routes in the graph that start at the given source node.
+    /// A [`DirectRoute`] does not contain the nodes traversed, only the source and target nodes.
     fn distance_from<'graph: 'this, 'this>(
         &'this self,
         graph: &'graph Graph<S>,
@@ -88,6 +204,10 @@ where
 
         Ok(iter.filter(move |route| route.source().id() == source))
     }
+
+    /// Returns the shortest distance from the source to the target.
+    ///
+    /// This will return [`None`] if no path exists between the source and the target.
     fn distance_between<'graph>(
         &self,
         graph: &'graph Graph<S>,
@@ -99,6 +219,13 @@ where
             .find(move |route| route.source().id() == source && route.target().id() == target)
             .map(|route| route.into_cost())
     }
+
+    /// Returns an iterator over all shortest distances in the graph.
+    ///
+    /// This is also known as `APSP` (All Pairs Shortest Path) problem.
+    ///
+    /// Returns an iterator over all direct routes in the graph.
+    /// A [`DirectRoute`] does not contain the nodes traversed, only the source and target nodes.
     fn every_distance<'graph: 'this, 'this>(
         &'this self,
         graph: &'graph Graph<S>,
