cd323968f3d02a07124a6e2a272ac9169e3af74f
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-12-26 19:21:05 +0100

    feat(algorithms): closure binders

--- crates/algorithms/src/shortest_paths/common/closures.rs
+++ crates/algorithms/src/shortest_paths/common/closures.rs
@@ -1,77 +1,62 @@
 use core::marker::PhantomData;
 
 use numi::borrow::Moo;
-use petgraph_core::{Edge, GraphStorage};
+use petgraph_core::{Edge, GraphStorage, Node};
 
 use crate::shortest_paths::GraphCost;
 
-struct GraphCostClosure<F, S, T> {
-    closure: F,
-    _marker: PhantomData<fn() -> *const (S, T)>,
+pub fn cost<S, T>(closure: impl Fn(Edge<S>) -> Moo<T>) -> impl Fn(Edge<S>) -> Moo<T> {
+    closure
 }
 
-impl<S, T> GraphCostClosure<(), S, T> {
-    pub fn new(
-        closure: impl Fn(Edge<S>) -> Moo<T>,
-    ) -> GraphCostClosure<impl Fn(Edge<S>) -> Moo<T>, S, T> {
-        GraphCostClosure {
-            closure,
-            _marker: PhantomData,
-        }
-    }
-}
-
-impl<F, S, T> GraphCostClosure<F, S, T>
+pub fn heuristic<S, T>(
+    closure: impl for<'graph> Fn(Node<'graph, S>, Node<'graph, S>) -> Moo<'graph, T>,
+) -> impl for<'graph> Fn(Node<'graph, S>, Node<'graph, S>) -> Moo<'graph, T>
 where
-    F: Fn(Edge<S>) -> Moo<T>,
-{
-    pub fn narrow_node_weight<N>(self) -> GraphCostClosure<impl Fn(Edge<S>) -> Moo<T>, S, T>
-    where
-        S: GraphStorage<NodeWeight = N>,
-    {
-        GraphCostClosure {
-            closure: self.closure,
-            _marker: PhantomData,
-        }
-    }
-
-    pub fn narrow_edge_weight<E>(self) -> GraphCostClosure<impl Fn(Edge<S>) -> Moo<T>, S, T>
-    where
-        S: GraphStorage<EdgeWeight = E>,
-    {
-        GraphCostClosure {
-            closure: self.closure,
-            _marker: PhantomData,
-        }
-    }
-}
-
-impl<F, S, T> GraphCost<S> for GraphCostClosure<F, S, T>
-where
-    F: Fn(Edge<S>) -> Moo<T>,
     S: GraphStorage,
 {
-    type Value = T;
-
-    fn cost<'graph>(&self, edge: Edge<'graph, S>) -> Moo<'graph, Self::Value> {
-        (self.closure)(edge)
-    }
+    closure
 }
 
 #[cfg(test)]
 mod test {
     use numi::borrow::Moo;
     use petgraph_core::Graph;
+    use petgraph_dino::DiDinoGraph;
 
-    use crate::shortest_paths::{common::closures::GraphCostClosure, Dijkstra};
+    use crate::shortest_paths::{
+        common::closures::{cost, heuristic},
+        AStar, Dijkstra, ShortestPath,
+    };
 
     #[test]
-    fn cost() {
-        let closure = GraphCostClosure::new(|edge| Moo::Borrowed(edge.weight()))
-            .narrow_edge_weight::<i32>()
-            .narrow_node_weight::<i32>();
+    fn bind_cost() {
+        let closure = cost(|edge| Moo::Borrowed(edge.weight()));
 
-        let closure = Dijkstra::undirected().with_edge_cost(closure);
+        let algorithm = Dijkstra::undirected().with_edge_cost(closure);
+
+        let mut graph = DiDinoGraph::new();
+        let a = *graph.insert_node("A").id();
+        let b = *graph.insert_node("B").id();
+
+        graph.insert_edge(7, &a, &b);
+
+        let path = algorithm.path_between(&graph, &a, &b).expect("path exists");
+    }
+
+    #[test]
+    fn bind_heuristic() {
+        let closure = heuristic(|source, target| Moo::Owned(0i32));
+
+        let algorithm = AStar::undirected().with_heuristic(closure);
+
+        let mut graph = DiDinoGraph::new();
+        let a = *graph.insert_node("A").id();
+        let b = *graph.insert_node("B").id();
+
+        graph.insert_edge(7i32, &a, &b);
+
+        let path = algorithm.path_between(&graph, &a, &b).expect("path exists");
     }
 }
 
