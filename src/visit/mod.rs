14baac2882a20eabbd0c315528725280cc28e87b
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2024-01-14 20:03:32 -0600

    Add unvisit to VisitMap
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- src/visit/mod.rs
+++ src/visit/mod.rs
@@ -398,6 +398,13 @@ pub trait VisitMap<N> {
 
     /// Return whether `a` has been visited before.
     fn is_visited(&self, a: &N) -> bool;
+
+    /// Mark `a` as unvisited.
+    /// 
+    /// Return **true** if this vertex was marked as visited at the time of unsetting it, false otherwise.
+    fn unvisit(&mut self, _a: N) -> bool {
+        unimplemented!("We don't know how to mark the node as unvisited.")
+    }
 }
 
 impl<Ix> VisitMap<Ix> for FixedBitSet
@@ -409,6 +416,14 @@ where
     }
     fn is_visited(&self, x: &Ix) -> bool {
         self.contains(x.index())
+    }
+
+    fn unvisit(&mut self, x: Ix) -> bool {
+        if self.is_visited(&x) {
+            self.toggle(x.index());
+            return true;
+        }
+        false
     }
 }
 
@@ -422,6 +437,10 @@ where
     }
     fn is_visited(&self, x: &N) -> bool {
         self.contains(x)
+    }
+
+    fn unvisit(&mut self, x: N) -> bool {
+        self.remove(&x)
     }
 }
 
