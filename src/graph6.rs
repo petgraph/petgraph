d5031747f9ce01bfb2d4be589617f5177b593dbd
BryanCruz <bryan.bialokur@gmail.com>
2024-06-02 19:03:19 -0300

    docs and refactors

--- src/graph6.rs
+++ src/graph6.rs
@@ -12,34 +12,20 @@ pub fn get_graph6_representation<G>(graph: G) -> String
 where
     G: GetAdjacencyMatrix + IntoNodeIdentifiers,
 {
-    let (graph_order, mut diagonal_bits) = get_adj_matrix_upper_diagonal_as_bits(graph);
-    let mut order_bits = get_graph_order_as_bits(graph_order);
+    let (graph_order, mut upper_diagonal_as_bits) = get_adj_matrix_upper_diagonal_as_bits(graph);
+    let mut graph_order_as_bits = get_graph_order_as_bits(graph_order);
 
     let mut graph_as_bits = vec![];
-    graph_as_bits.append(&mut order_bits);
-    graph_as_bits.append(&mut diagonal_bits);
+    graph_as_bits.append(&mut graph_order_as_bits);
+    graph_as_bits.append(&mut upper_diagonal_as_bits);
 
     bits_to_ascii(graph_as_bits)
 }
 
-fn bits_to_ascii(bits: Vec<usize>) -> String {
-    let bits_strs = bits.iter().map(|bit| bit.to_string()).collect::<Vec<_>>();
-
-    let bytes = bits_strs
-        .chunks(6)
-        .map(|bits_chunk| bits_chunk.join(""))
-        .map(|bits_str| usize::from_str_radix(&bits_str, 2));
-
-    bytes
-        .map(|byte| char::from((N + byte.unwrap()) as u8))
-        .collect()
-}
-
-// Traverse graph nodes and build the upper diagonal of its adjacency matrix.
+// Traverse graph nodes and construct the upper diagonal of its adjacency matrix as a vector of bits.
 // Returns a tuple containing:
 // - `n`: graph order (number of nodes in graph)
 // - `bits`: a vector of 0s and 1s encoding the upper diagonal of the graphs adjacency matrix.
-//           This will be normalized to have a length divisible by 6.
 fn get_adj_matrix_upper_diagonal_as_bits<G>(graph: G) -> (usize, Vec<usize>)
 where
     G: GetAdjacencyMatrix + IntoNodeIdentifiers,
@@ -62,20 +48,15 @@ where
         n += 1;
     }
 
-    while bits.len() % 6 != 0 {
-        bits.push(0);
-    }
-
     return (n, bits);
 }
 
+// Construct graph order as a vector of bits.
 fn get_graph_order_as_bits(order: usize) -> Vec<usize> {
     let to_convert_to_bits = if order < N {
         vec![(order, 6)]
     } else if order <= 258047 {
         vec![(N, 6), (order, 18)]
-    } else if order <= 68719476735 {
-        vec![(N, 6), (N, 6), (order, 36)]
     } else {
         panic!("Graph order not supported.")
     };
@@ -86,10 +67,30 @@ fn get_graph_order_as_bits(order: usize) -> Vec<usize> {
         .collect()
 }
 
-fn get_number_as_bits(n: usize, n_of_bits: usize) -> Vec<usize> {
+// Get binary representation of `n` as a vector of bits with `bits_length` length.
+fn get_number_as_bits(n: usize, bits_length: usize) -> Vec<usize> {
     let mut bits = Vec::new();
-    for i in (0..n_of_bits).rev() {
+    for i in (0..bits_length).rev() {
         bits.push((n >> i) & 1);
     }
     bits
+}
+
+// Convert a vector of bits to a String using ASCII encoding.
+// Each 6 bits will be converted to a single ASCII character.
+fn bits_to_ascii(mut bits: Vec<usize>) -> String {
+    while bits.len() % 6 != 0 {
+        bits.push(0);
+    }
+
+    let bits_strs = bits.iter().map(|bit| bit.to_string()).collect::<Vec<_>>();
+
+    let bytes = bits_strs
+        .chunks(6)
+        .map(|bits_chunk| bits_chunk.join(""))
+        .map(|bits_str| usize::from_str_radix(&bits_str, 2));
+
+    bytes
+        .map(|byte| char::from((N + byte.unwrap()) as u8))
+        .collect()
 }
