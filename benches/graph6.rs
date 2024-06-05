182b035d190680d11b8e66013fdb65da1d8b06e5
BryanCruz <bryan.bialokur@gmail.com>
2024-06-04 22:54:42 -0300

    add graph6 benches

---
+++ benches/graph6.rs
@@ -0,0 +1,35 @@
+#![feature(test)]
+
+extern crate petgraph;
+extern crate test;
+
+use petgraph::graph6::Graph6;
+use test::Bencher;
+
+#[allow(dead_code)]
+mod common;
+use common::ungraph;
+
+#[bench]
+fn graph6_string_praust_bench(bench: &mut Bencher) {
+    let a = ungraph().praust_a();
+    let b = ungraph().praust_b();
+
+    bench.iter(|| (a.graph6_string(), b.graph6_string()));
+}
+
+#[bench]
+fn graph6_string_full_bench(bench: &mut Bencher) {
+    let a = ungraph().full_a();
+    let b = ungraph().full_b();
+
+    bench.iter(|| (a.graph6_string(), b.graph6_string()));
+}
+
+#[bench]
+fn graph6_string_petersen_bench(bench: &mut Bencher) {
+    let a = ungraph().petersen_a();
+    let b = ungraph().petersen_b();
+
+    bench.iter(|| (a.graph6_string(), b.graph6_string()));
+}
