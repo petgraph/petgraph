82fa57ef7a8f7a1c15113fbfacdd29bd9dbce6a1
BryanCruz <bryan.bialokur@gmail.com>
2024-07-10 17:56:22 -0300

    add more test cases

--- tests/min_spanning_tree.rs
+++ tests/min_spanning_tree.rs
@@ -1,7 +1,9 @@
 use petgraph::{
     algo::{min_spanning_tree, min_spanning_tree_prim},
+    csr::IndexType,
     dot::Dot,
-    graph::UnGraph,
+    graph::{NodeIndex, UnGraph},
+    visit::{IntoEdges, NodeRef},
     Graph, Undirected,
 };
 
@@ -163,3 +165,63 @@ fn mst_prim_empty_graph() {
     assert!(mst.node_count() == 0);
     assert!(mst.edge_count() == 0);
 }
+
+#[test]
+fn mst_kruskal_test_cases() {
+    use petgraph::data::FromElements;
+
+    for (edges, expected_mst_edges) in TEST_CASES {
+        let mut gr = UnGraph::new_undirected();
+        gr.extend_with_edges(edges.to_vec());
+
+        let mst: Graph<(), u32, Undirected, u32> = UnGraph::from_elements(min_spanning_tree(&gr));
+
+        assert!(mst.node_count() == gr.node_count());
+        assert!(mst.edge_count() == expected_mst_edges.len());
+
+        for (source, target, _) in expected_mst_edges {
+            let a = NodeIndex::new(*source as usize);
+            let b = NodeIndex::new(*target as usize);
+            assert!(mst.contains_edge(a, b));
+        }
+    }
+}
+
+#[test]
+fn mst_prim_test_cases() {
+    use petgraph::data::FromElements;
+
+    for (edges, expected_mst_edges) in TEST_CASES {
+        let mut gr = UnGraph::new_undirected();
+        gr.extend_with_edges(edges.to_vec());
+
+        let mst: Graph<(), u32, Undirected, u32> =
+            UnGraph::from_elements(min_spanning_tree_prim(&gr));
+
+        assert!(mst.node_count() == gr.node_count());
+        assert!(mst.edge_count() == expected_mst_edges.len());
+
+        for (source, target, _) in expected_mst_edges {
+            let a = NodeIndex::new(*source as usize);
+            let b = NodeIndex::new(*target as usize);
+            assert!(mst.contains_edge(a, b));
+        }
+    }
+}
+
+// Test cases format: (graph order, graph edges, mst edges)
+#[rustfmt::skip]
+const TEST_CASES: [(&[(u32, u32, u32)], &[(u32, u32, u32)]); 4] = [
+    // Order 9
+    (&[(0, 2, 107), (0, 3, 24), (0, 4, 47), (0, 5, 60), (0, 6, 98), (0, 7, 29), (0, 8, 20), (1, 2, 62), (1, 3, 115), (1, 6, 42), (1, 7, 117), (1, 8, 19), (2, 3, 39), (2, 4, 12), (2, 5, 27), (2, 7, 3), (2, 8, 66), (3, 4, 54), (3, 5, 129), (3, 6, 18), (3, 7, 137), (3, 8, 120), (4, 5, 9), (4, 6, 124), (4, 7, 103), (4, 8, 7), (5, 7, 77), (5, 8, 63), (6, 7, 130), (7, 8, 138)],
+    &[(2, 7, 3), (4, 8, 7), (4, 5, 9), (2, 4, 12), (3, 6, 18), (1, 8, 19), (0, 8, 20), (0, 3, 24)]),
+    // Order 10
+    (&[(0, 2, 84), (0, 4, 5), (0, 5, 17), (0, 6, 97), (0, 7, 74), (0, 8, 16), (1, 3, 49), (1, 4, 28), (1, 8, 51), (1, 9, 137), (2, 3, 125), (2, 4, 87), (2, 6, 114), (2, 8, 131), (3, 4, 136), (3, 5, 43), (3, 8, 24), (3, 9, 112), (4, 5, 61), (4, 7, 99), (4, 8, 63), (4, 9, 108), (5, 6, 13), (5, 7, 9), (5, 8, 133), (6, 9, 147), (7, 8, 10), (7, 9, 88), (8, 9, 68)],
+    &[(0, 4, 5), (5, 7, 9), (7, 8, 10), (5, 6, 13), (0, 8, 16), (3, 8, 24), (1, 4, 28), (8, 9, 68), (0, 2, 84)]),
+    // Order 15
+    (&[(0, 1, 124), (0, 3, 126), (0, 6, 84), (0, 7, 87), (0, 9, 93), (0, 12, 32), (1, 2, 51), (1, 4, 144), (1, 6, 36), (1, 8, 46), (1, 9, 8), (1, 13, 26), (2, 8, 111), (3, 4, 114), (3, 6, 98), (3, 8, 86), (3, 9, 73), (4, 5, 41), (4, 6, 7), (4, 8, 82), (4, 9, 48), (4, 10, 113), (4, 11, 54), (4, 12, 10), (5, 8, 60), (5, 13, 34), (6, 13, 85), (6, 14, 52), (7, 8, 74), (7, 12, 137), (8, 10, 118), (8, 12, 69), (8, 13, 133), (9, 12, 13), (10, 12, 65), (10, 14, 107), (11, 14, 102), (12, 13, 140), (12, 14, 11), (13, 14, 25)],
+    &[(4, 6, 7), (1, 9, 8), (4, 12, 10), (12, 14, 11), (9, 12, 13), (13, 14, 25), (0, 12, 32), (5, 13, 34), (1, 8, 46), (1, 2, 51), (4, 11, 54), (10, 12, 65), (3, 9, 73), (7, 8, 74)]),
+    // Order 20
+    (&[(0, 2, 5), (0, 19, 73), (0, 12, 3), (1, 17, 145), (1, 18, 16), (1, 3, 125), (1, 5, 6), (1, 10, 76), (1, 11, 13), (1, 15, 12), (2, 7, 34), (2, 9, 118), (3, 12, 43), (3, 13, 146), (4, 7, 31), (5, 6, 62), (5, 8, 147), (6, 14, 66), (7, 16, 67), (7, 17, 48), (7, 10, 93), (7, 12, 113), (7, 14, 85), (8, 16, 40), (8, 18, 111), (9, 17, 102), (10, 16, 128), (10, 18, 120), (11, 17, 35), (11, 18, 88), (11, 13, 54), (11, 14, 36), (12, 16, 148), (13, 15, 75), (16, 17, 71), (16, 18, 10)],
+    &[(0, 12, 3), (0, 2, 5), (1, 5, 6), (16, 18, 10), (1, 15, 12), (1, 11, 13), (1, 18, 16), (4, 7, 31), (2, 7, 34), (11, 17, 35), (11, 14, 36), (8, 16, 40), (3, 12, 43), (7, 17, 48), (11, 13, 54), (5, 6, 62), (0, 19, 73), (1, 10, 76), (9, 17, 102)]),
+];
