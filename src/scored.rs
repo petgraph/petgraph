159341e4af2a1292d8a1da428d64784e2dfc8ae5
Egor Starovoitov <52821033+starovoid@users.noreply.github.com>
2024-12-29 19:23:48 +0300

    Implement DSatur graph coloring algorithm
    - Implemented DSatur graph coloring algorithm.
    
    After several simple optimizations, the performance on the existing
    benchmark has improved:
    - Old: `test dsatur_coloring_bench ... bench: 13,808,991.80 ns/iter (+/-
    294,797.19)`
    - New: `test dsatur_coloring_bench ... bench: 9,900,469.20 ns/iter (+/-
    289,755.02)`
    
    To make optimizations, we had to add the `NodeIndexable` trait
    requirement. But I think it's a small price to pay for improved
    performance, and it won't hurt at all.
    
    ---------
    
    Co-authored-by: Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>

--- src/scored.rs
+++ src/scored.rs
@@ -50,3 +50,44 @@ impl<K: PartialOrd, T> Ord for MinScored<K, T> {
         }
     }
 }
+
+#[derive(Copy, Clone, Debug)]
+pub struct MaxScored<K, T>(pub K, pub T);
+
+impl<K: PartialOrd, T> PartialEq for MaxScored<K, T> {
+    #[inline]
+    fn eq(&self, other: &MaxScored<K, T>) -> bool {
+        self.cmp(other) == Ordering::Equal
+    }
+}
+
+impl<K: PartialOrd, T> Eq for MaxScored<K, T> {}
+
+impl<K: PartialOrd, T> PartialOrd for MaxScored<K, T> {
+    #[inline]
+    fn partial_cmp(&self, other: &MaxScored<K, T>) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<K: PartialOrd, T> Ord for MaxScored<K, T> {
+    #[inline]
+    fn cmp(&self, other: &MaxScored<K, T>) -> Ordering {
+        let a = &self.0;
+        let b = &other.0;
+        if a == b {
+            Ordering::Equal
+        } else if a < b {
+            Ordering::Less
+        } else if a > b {
+            Ordering::Greater
+        } else if a.ne(a) && b.ne(b) {
+            // these are the NaN cases
+            Ordering::Equal
+        } else if a.ne(a) {
+            Ordering::Less
+        } else {
+            Ordering::Greater
+        }
+    }
+}
