d65114eafd432a4527a1095e0ae5b27e75811e5c
Raoul Luqué <raoulsluque@gmail.com>
2025-05-27 15:57:42 +0200

    Rebase

--- src/algo/chordal.rs
+++ src/algo/chordal.rs
@@ -5,6 +5,80 @@ use alloc::{vec, vec::Vec};
 use core::hash::Hash;
 use hashbrown::HashSet;
 
+/// Checks if the given graph is [chordal][1].
+///
+/// A chordal graph is a graph in which every cycle of four or more vertices has a chord,
+/// which is an edge that is not part of the cycle but connects two vertices of the cycle.
+///
+/// The provided graph is assumed to be undirected and [simple][2].
+///
+/// The time complexity of this implementation is O(V + E).
+/// This is achieved by computing a [maximum cardinality search][3] ordering and checking
+/// if it is a [perfect elimination ordering][4].
+///
+/// ## Arguments
+/// * `graph`: The undirected graph to check.
+///
+/// ## Returns
+/// * `true`: If the graph is chordal.
+/// * `false`: If the graph is not chordal.
+///
+/// [1]: https://en.wikipedia.org/wiki/Chordal_graph
+/// [2]: https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)#Graph
+/// [3]: https://epubs.siam.org/doi/10.1137/0213035
+/// [4]: https://en.wikipedia.org/wiki/Chordal_graph#Perfect_elimination_and_efficient_recognition
+///
+/// # Examples
+///
+/// ```
+/// use petgraph::algo::is_chordal;
+/// use petgraph::{Graph, Undirected};
+///
+/// let mut graph: Graph<(), (), Undirected> = Graph::new_undirected();
+/// let a = graph.add_node(());
+/// let b = graph.add_node(());
+/// let c = graph.add_node(());
+/// let d = graph.add_node(());
+///
+/// graph.extend_with_edges(&[
+///    (a, b, ()),
+///    (a, c, ()),
+///    (a, d, ()),
+///    (b, c, ()),
+///    (b, d, ()),
+///    (c, d, ())
+/// ]);
+///
+/// // Graph is chordal:
+/// //   --- b ---
+/// //  |    |    |
+/// //  |    |    |
+/// //  |    a -- c
+/// //  |    |    |
+/// //  |    |    |
+/// //   --- d ---
+///
+/// assert!(is_chordal(&graph));
+///
+/// let e = graph.add_node(());
+/// graph.remove_edge(graph.find_edge(b, d).unwrap());
+/// graph.extend_with_edges(&[
+///     (b, e, ()),
+///     (e, d, ()),
+/// ]);
+///
+/// // Graph is not chordal anymore
+/// // (see the cycle a-b-e-d-a):
+/// //   --- b ---
+/// //  |    |    |
+/// //  |    |    |
+/// //  e    a -- c
+/// //  |    |    |
+/// //  |    |    |
+/// //   --- d ---
+///
+/// assert!(!is_chordal(&graph));
+/// ```
 pub fn is_chordal<G>(graph: G) -> bool
 where
     G: IntoNeighbors + IntoNodeIdentifiers + NodeCount + NodeIndexable + Visitable,
@@ -16,10 +90,10 @@ where
     }
 
     // Compute the maximum cardinality search ordering
-    let mcs_ordering = maximum_cardinality_search(&graph);
+    let mcs_ordering = maximum_cardinality_search(graph);
 
     // Check if the ordering is a perfect elimination ordering
-    is_perfect_elimination_ordering(&graph, &mcs_ordering)
+    is_perfect_elimination_ordering(graph, &mcs_ordering)
 }
 
 /// State struct for the maximum cardinality search (MCS) algorithm.
@@ -50,7 +124,8 @@ impl<G> MCSState<G>
 where
     G: NodeIndexable + Visitable,
 {
-    fn new(graph: &G, num_nodes: usize, node_bound: usize) -> Self {
+    /// Creates a new MCS state for the given graph.
+    fn new(graph: G, num_nodes: usize, node_bound: usize) -> Self {
         MCSState {
             elimination_ord: vec![graph.from_index(0); num_nodes],
             current_ord_index: num_nodes - 1,
@@ -62,48 +137,83 @@ where
         }
     }
 
-    fn update_label(&mut self, graph: &G, neighbor: G::NodeId) {
-        let neighbor_index = graph.to_index(neighbor);
-        let neighbor_label = self.num_sel[neighbor_index];
-        let inner_bucket_last_element = *self.buckets[neighbor_label]
+    /// Updates the label of a node in the MCS state. That is, increments the
+    /// number of already selected neighbors of the node and moves it to the
+    /// next bucket.
+    fn update_label(&mut self, graph: G, node: G::NodeId) {
+        let node_index = graph.to_index(node);
+        let node_label = self.num_sel[node_index];
+        let inner_bucket_last_element = *self.buckets[node_label]
             .last()
-            .expect("Inner bucket shouldn't be empty since neighbor is in it");
-        let neighbor_inner_bucket_index = self.inner_idx[neighbor_index];
+            .expect("Inner bucket {} shouldn't be empty, since node is in it");
+        let node_inner_bucket_index = self.inner_idx[node_index];
         self.buckets
-            .get_mut(neighbor_label)
+            .get_mut(node_label)
             .expect("Inner bucket should exist")
-            .swap_remove(neighbor_inner_bucket_index);
+            .swap_remove(node_inner_bucket_index);
 
-        if neighbor != inner_bucket_last_element {
-            self.inner_idx[graph.to_index(inner_bucket_last_element)] = neighbor_inner_bucket_index;
+        if node != inner_bucket_last_element {
+            self.inner_idx[graph.to_index(inner_bucket_last_element)] = node_inner_bucket_index;
         }
 
-        self.num_sel[neighbor_index] = neighbor_label + 1;
-        if neighbor_label + 1 > self.current_high {
-            self.current_high = neighbor_label + 1;
+        self.num_sel[node_index] = node_label + 1;
+        if node_label + 1 > self.current_high {
+            self.current_high = node_label + 1;
         }
         self.buckets
-            .get_mut(neighbor_label + 1)
+            .get_mut(node_label + 1)
             .expect("Inner bucket should exist")
-            .push(neighbor);
-        self.inner_idx[neighbor_index] = self.buckets[neighbor_label + 1].len() - 1;
+            .push(node);
+        self.inner_idx[node_index] = self.buckets[node_label + 1].len() - 1;
     }
 }
 
-/// Computes an elimination ordering of a graph using the maximum cardinality
-/// search (MCS) algorithm.
-/// The algorithm was first proposed by [Tarjan and Yannakakis][1] in 1984.
+/// [Maximum cardinality search (MCS) algorithm][1] for computing elimination
+/// orderings.
 ///
 /// If the graph is [chordal][2], MCS will return a [perfect elimination ordering][3].
 ///
-/// The runtime of the algorithm and this implementation in particular is
-/// O(n + m), where n is the number of nodes and m is the number of edges
-/// in the graph. This implementation achieves this runtime.
+/// The time complexity of the algorithm and this implementation in particular is
+/// O(V + E).
+///
+/// ## Arguments
+/// * `graph`: The graph to compute the ordering for.
+///
+/// ## Returns
+/// * A vector containing the nodes in the order of the elimination ordering.
 ///
 /// [1]: https://epubs.siam.org/doi/10.1137/0213035
 /// [2]: https://en.wikipedia.org/wiki/Chordal_graph
 /// [3]: https://en.wikipedia.org/wiki/Chordal_graph#Perfect_elimination_and_efficient_recognition
-pub fn maximum_cardinality_search<G>(graph: &G) -> Vec<G::NodeId>
+///
+/// # Examples
+///
+/// ```
+/// use petgraph::algo::maximum_cardinality_search;
+/// use petgraph::{Graph, Undirected};
+///
+/// let mut graph: Graph<(), (), Undirected> = Graph::new_undirected();
+/// let a = graph.add_node(());
+/// let b = graph.add_node(());
+/// let c = graph.add_node(());
+/// let d = graph.add_node(());
+///
+/// graph.extend_with_edges(&[(a, c, ()), (a, d, ()), (b, c, ()), (b, d, ()), (c, d, ())]);
+///
+/// // Graph:
+/// //  b --- ---
+/// //  |         |
+/// //  |         |
+/// //  |      -- c
+/// //  |    |    |
+/// //  |    |    |
+/// //   --- d -- a
+///
+/// let elimination_ordering = maximum_cardinality_search(&graph);
+/// assert_eq!(elimination_ordering.len(), graph.node_count());
+/// assert_eq!(elimination_ordering, vec![b, d, c, a]);
+/// ```
+pub fn maximum_cardinality_search<G>(graph: G) -> Vec<G::NodeId>
 where
     G: IntoNeighbors + IntoNodeIdentifiers + NodeCount + NodeIndexable + Visitable,
 {
@@ -114,8 +224,8 @@ where
     }
 
     // Take an arbitrary node as the last node in the ordering
-    let mut main_node_iter = graph.node_identifiers();
-    let last = main_node_iter
+    let last = graph
+        .node_identifiers()
         .next()
         .expect("Graph is not empty due to check");
 
@@ -136,8 +246,6 @@ where
             .expect("Bucket should exist")
             .push(node);
         mcs_state.inner_idx[node_index] = index;
-        mcs_state.current_high += 1;
-        todo!();
     }
 
     // Insert the last node at the start of the ordering and update the count of its neighbors
@@ -158,12 +266,15 @@ where
 
         // Insert the node at the start of the ordering
         mcs_state.elimination_ord[mcs_state.current_ord_index] = highest_label_node;
-        mcs_state.current_ord_index -= 1;
-        mcs_state.visited.visit(last);
+        mcs_state.current_ord_index = mcs_state.current_ord_index.saturating_sub(1);
+        mcs_state.visited.visit(highest_label_node);
 
         // Check if highest bucket is now empty
         if mcs_state.buckets[mcs_state.current_high].is_empty() {
-            mcs_state.current_high -= 1;
+            while mcs_state.buckets[mcs_state.current_high].is_empty() && mcs_state.current_high > 0
+            {
+                mcs_state.current_high -= 1;
+            }
         }
 
         // Update the labels of the neighbors of the highest label node
@@ -178,27 +289,33 @@ where
     mcs_state.elimination_ord
 }
 
-fn is_perfect_elimination_ordering<G>(graph: &G, ordering: &[G::NodeId]) -> bool
+/// Checks if the given ordering is a [perfect elimination ordering][1] for the graph.
+/// This is done in O(n + m) time, where n is the number of nodes and m is the number of edges.
+///
+/// [1]: https://en.wikipedia.org/wiki/Chordal_graph#Perfect_elimination_and_efficient_recognition
+pub fn is_perfect_elimination_ordering<G>(graph: G, ordering: &[G::NodeId]) -> bool
 where
     G: IntoNeighbors + Visitable,
     G::NodeId: Eq + Hash,
 {
     let mut eliminated = graph.visit_map();
-    for i in (0..ordering.len()).rev() {
-        let node = ordering[i];
-        eliminated.visit(node);
+    for node in ordering {
+        eliminated.visit(*node);
         let neighbors_not_visited = graph
-            .neighbors(node)
-            .filter(|n| !eliminated.is_visited(&n))
+            .neighbors(*node)
+            .filter(|n| !eliminated.is_visited(n))
             .collect::<HashSet<_>>();
-        if !is_clique(&graph, neighbors_not_visited) {
+        // Check if the neighbors of the node that are not yet eliminated form a clique.
+        // Otherwise, the ordering is not a perfect elimination ordering
+        if !is_clique(graph, neighbors_not_visited) {
             return false;
         }
     }
     true
 }
 
-fn is_clique<G>(graph: &G, nodes: HashSet<G::NodeId>) -> bool
+/// Checks if the given set of nodes form a clique in the given graph.
+fn is_clique<G>(graph: G, nodes: HashSet<G::NodeId>) -> bool
 where
     G: IntoNeighbors,
     G::NodeId: Eq + Hash,
