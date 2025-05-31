76cb91b56ca5d5a30874a9cc9963ce535202b873
Raoul Luqué <raoulsluque@gmail.com>
2025-05-31 13:28:12 +0200

    Add example to minimum spanning tree methods

--- src/algo/min_spanning_tree.rs
+++ src/algo/min_spanning_tree.rs
@@ -24,9 +24,58 @@ use crate::visit::{IntoEdgeReferences, NodeIndexable};
 ///
 /// Use `from_elements` to create a graph from the resulting iterator.
 ///
-/// See also: [`.min_spanning_tree_prim(g)`][1] for implementation using Prim's algorithm.
+/// See also: [`min_spanning_tree_prim`][1] for an implementation using Prim's algorithm.
 ///
 /// [1]: fn.min_spanning_tree_prim.html
+///
+/// # Example
+/// ```rust
+/// use petgraph::Graph;
+/// use petgraph::algo::min_spanning_tree;
+/// use petgraph::data::FromElements;
+/// use petgraph::graph::UnGraph;
+///
+/// let mut g = Graph::new_undirected();
+/// let a = g.add_node(());
+/// let b = g.add_node(());
+/// let c = g.add_node(());
+/// let d = g.add_node(());
+/// let e = g.add_node(());
+/// let f = g.add_node(());
+/// g.extend_with_edges(&[
+///     (0, 1, 2.0),
+///     (0, 3, 4.0),
+///     (1, 2, 1.0),
+///     (1, 5, 7.0),
+///     (2, 4, 5.0),
+///     (4, 5, 1.0),
+///     (3, 4, 1.0),
+/// ]);
+///
+/// // The graph looks like this:
+/// //     2       1
+/// // a ----- b ----- c
+/// // | 4     | 7     |
+/// // d       f       | 5
+/// // | 1     | 1     |
+/// // \------ e ------/
+///
+/// let mst = UnGraph::<_, _>::from_elements(min_spanning_tree(&g));
+/// assert_eq!(g.node_count(), mst.node_count());
+/// assert_eq!(mst.node_count() - 1, mst.edge_count());
+///
+/// // The resulting minimum spanning tree looks like this:
+/// //     2       1
+/// // a ----- b ----- c
+/// // | 4             
+/// // d       f       
+/// // | 1     | 1       
+/// // \------ e
+///
+/// let mut edge_weight_vec = mst.edge_weights().cloned().collect::<Vec<_>>();
+/// edge_weight_vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
+/// assert_eq!(edge_weight_vec , vec![1.0, 1.0, 1.0, 2.0, 4.0]);
+/// ```
 pub fn min_spanning_tree<G>(g: G) -> MinSpanningTree<G>
 where
     G::NodeWeight: Clone,
@@ -141,6 +190,55 @@ where
 /// See also: [`.min_spanning_tree(g)`][1] for implementation using Kruskal's algorithm and support for minimum spanning forest.
 ///
 /// [1]: fn.min_spanning_tree.html
+///
+/// # Example
+/// ```rust
+/// use petgraph::Graph;
+/// use petgraph::algo::min_spanning_tree_prim;
+/// use petgraph::data::FromElements;
+/// use petgraph::graph::UnGraph;
+///
+/// let mut g = Graph::new_undirected();
+/// let a = g.add_node(());
+/// let b = g.add_node(());
+/// let c = g.add_node(());
+/// let d = g.add_node(());
+/// let e = g.add_node(());
+/// let f = g.add_node(());
+/// g.extend_with_edges(&[
+///     (0, 1, 2.0),
+///     (0, 3, 4.0),
+///     (1, 2, 1.0),
+///     (1, 5, 7.0),
+///     (2, 4, 5.0),
+///     (4, 5, 1.0),
+///     (3, 4, 1.0),
+/// ]);
+///
+/// // The graph looks like this:
+/// //     2       1
+/// // a ----- b ----- c
+/// // | 4     | 7     |
+/// // d       f       | 5
+/// // | 1     | 1     |
+/// // \------ e ------/
+///
+/// let mst = UnGraph::<_, _>::from_elements(min_spanning_tree_prim(&g));
+/// assert_eq!(g.node_count(), mst.node_count());
+/// assert_eq!(mst.node_count() - 1, mst.edge_count());
+///
+/// // The resulting minimum spanning tree looks like this:
+/// //     2       1
+/// // a ----- b ----- c
+/// // | 4
+/// // d       f
+/// // | 1     | 1
+/// // \------ e
+///
+/// let mut edge_weight_vec = mst.edge_weights().cloned().collect::<Vec<_>>();
+/// edge_weight_vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
+/// assert_eq!(edge_weight_vec , vec![1.0, 1.0, 1.0, 2.0, 4.0]);
+/// ```
 pub fn min_spanning_tree_prim<G>(g: G) -> MinSpanningTreePrim<G>
 where
     G::EdgeWeight: PartialOrd,
