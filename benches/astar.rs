08619b97defb4c53bd0563eee7d929685a245b1b
Dietrich Daroch <Dietrich@Daroch.me>
2025-06-02 20:02:52 -0400

    test: Add a 2D Maze benchmark for A* and Dijkstra

---
+++ benches/astar.rs
@@ -0,0 +1,56 @@
+#![feature(test)]
+
+extern crate petgraph;
+extern crate test;
+
+use petgraph::prelude::*;
+use test::Bencher;
+
+mod common;
+use petgraph::algo::astar;
+
+#[bench]
+fn astar_maze2d_bench(bench: &mut Bencher) {
+    // Same maze as ./dijkstra.rs
+    const N: usize = 500;
+    let g = common::build_2d_maze(N, N);
+
+    let heuristic_for = |goal: NodeIndex| {
+        let g = &g;
+        move |node: NodeIndex| -> usize {
+            let (x1, y1): (usize, usize) = g[node];
+            let (x2, y2): (usize, usize) = g[goal];
+
+            x2.abs_diff(x1) + y2.abs_diff(y1)
+        }
+    };
+
+    let start_i = 0;
+    let goal_i = N * N - 1;
+    let start = NodeIndex::new(start_i);
+    let goal = NodeIndex::new(goal_i);
+
+    if let Some((cost, path)) = astar(
+        &g,
+        start,
+        |n| n == goal,
+        |e| *e.weight(),
+        heuristic_for(goal),
+    ) {
+        assert_eq!(cost, 2 * (N - 1));
+
+        assert_eq!(path.first().unwrap().index(), start_i);
+        assert_eq!(path.last().unwrap().index(), goal_i);
+        assert_eq!(path.len(), cost + 1);
+    }
+
+    bench.iter(|| {
+        let _ = astar(
+            &g,
+            start,
+            |n| n == goal,
+            |e| *e.weight(),
+            heuristic_for(goal),
+        );
+    });
+}
