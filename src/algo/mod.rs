0864493739b0cdccebf5406d594d8a7c905516d7
AnarchistHoneybun <thatonebipanda@gmail.com>
2025-06-14 21:57:28 +0530

    update exports et cetera to accomodate wfc

--- src/algo/mod.rs
+++ src/algo/mod.rs
@@ -44,7 +44,7 @@ use crate::visit::Walker;
 pub use astar::astar;
 pub use bellman_ford::{bellman_ford, find_negative_cycle};
 pub use bridges::bridges;
-pub use coloring::dsatur_coloring;
+pub use coloring::{dsatur_coloring, wfc_coloring};
 pub use dijkstra::dijkstra;
 pub use feedback_arc_set::greedy_feedback_arc_set;
 pub use floyd_warshall::floyd_warshall;
