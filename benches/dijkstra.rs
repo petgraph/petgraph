66d96b71adb74c504490e3c354ce21afc744846a
Rico Hageman <info@ricohageman.nl>
2025-09-16 20:48:01 +0200

    Update the benchmarks

--- benches/dijkstra.rs
+++ benches/dijkstra.rs
@@ -3,29 +3,14 @@
 extern crate petgraph;
 extern crate test;
 
-use core::cmp::{max, min};
+#[allow(dead_code)]
+mod common;
+
 use petgraph::prelude::*;
-use test::Bencher;
-
 use petgraph::algo::{bidirectional_dijkstra, dijkstra};
+use test::Bencher;
+use common::*;
 
-#[allow(clippy::needless_range_loop)]
-fn build_random_graph(node_count: usize) -> Graph<usize, usize, Undirected> {
-    let mut graph = Graph::new_undirected();
-    let nodes: Vec<NodeIndex<_>> = (0..node_count).map(|i| graph.add_node(i)).collect();
-    for i in 0..node_count {
-        let n1 = nodes[i];
-        let neighbour_count = i % 8 + 3;
-        let j_from = max(0, i as i32 - neighbour_count as i32 / 2) as usize;
-        let j_to = min(node_count, j_from + neighbour_count);
-        for j in j_from..j_to {
-            let n2 = nodes[j];
-            let distance = (i + 3) % 10;
-            graph.add_edge(n1, n2, distance);
-        }
-    }
-    graph
-}
 
 const RANDOM_GRAPH_SIZE: usize = 10_000;
 const RANDOM_GRAPH_START: usize = 1;
@@ -33,7 +18,7 @@ const RANDOM_GRAPH_GOAL: usize = 9_000;
 
 #[bench]
 fn dijkstra_bench_random(bench: &mut Bencher) {
-    let g = build_random_graph(RANDOM_GRAPH_SIZE);
+    let g = build_graph(RANDOM_GRAPH_SIZE, false);
 
     bench.iter(|| {
         let _scores = dijkstra(&g, NodeIndex::new(RANDOM_GRAPH_START), None, |e| {
@@ -44,7 +29,7 @@ fn dijkstra_bench_random(bench: &mut Bencher) {
 
 #[bench]
 fn dijkstra_bench_random_with_target(bench: &mut Bencher) {
-    let g = build_random_graph(RANDOM_GRAPH_SIZE);
+    let g = build_graph(RANDOM_GRAPH_SIZE, false);
 
     bench.iter(|| {
         let _scores = dijkstra(
@@ -58,7 +43,7 @@ fn dijkstra_bench_random_with_target(bench: &mut Bencher) {
 
 #[bench]
 fn dijkstra_bench_random_bidirectional(bench: &mut Bencher) {
-    let g = build_random_graph(RANDOM_GRAPH_SIZE);
+    let g = build_graph(RANDOM_GRAPH_SIZE, false);
 
     bench.iter(|| {
         let _result = bidirectional_dijkstra(
