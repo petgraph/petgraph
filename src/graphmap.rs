850831d05f9160f2cbe499b768d961bc5ded2886
Leni Aniva <a@leni.sh>
2025-10-28 11:24:40 -0700

    feat: Implement `ExactSizeIterator` for `AllEdges`

--- src/graphmap.rs
+++ src/graphmap.rs
@@ -913,6 +913,13 @@ where
             .map(|(&(n1, n2), weight)| (n1, n2, weight))
     }
 }
+impl<'a, N, E, Ty> ExactSizeIterator for AllEdges<'a, N, E, Ty>
+where
+    N: 'a + NodeTrait,
+    E: 'a,
+    Ty: EdgeType,
+{
+}
 
 impl<'a, N, E, Ty> DoubleEndedIterator for AllEdges<'a, N, E, Ty>
 where
