e543e29555fecfac1102c4f5ac13ab3283b754b1
Raoul Luqué <raoulsluque@gmail.com>
2026-02-21 16:37:17 +0100

    docs: Add docs for flow module

--- crates/algorithms/src/flows/mod.rs
+++ crates/algorithms/src/flows/mod.rs
@@ -1,1 +1,11 @@
+//! # Flows
+//! 
+//! In this module, we provide algorithms to solve various flow problems in graphs.
+//! [Flow problems](flow_problems_wikipedia), are a class of problems in which the input is a flow
+//! network (a graph with attributes like capacity, cost or demand on its edges), and the goal is to
+//! construct a feasible flow. That is assign a value to each edge which respects the constraints
+//! and possibly optimizes some objective function (like minimal cost).
+//!
+//! [flow_problems_wikipedia]: https://en.wikipedia.org/wiki/Network_flow_problem
+
 pub mod maximum_flow;
